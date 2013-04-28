ac3412233520e747ad88b7eda520b58c396cc4bd
Kai Braaten <kai.braaten@gmail.com>
2013-04-28 01:49:52 +0000

    Started moving ship stuff into ships.c

--- src/planets.c
+++ src/planets.c
@@ -18,7 +18,7 @@
  * Original Diku Mud copyright (C) 1990, 1991 by Sebastian Hammer,          *
  * Michael Seifert, Hans Henrik St{rfeldt, Tom Madsen, and Katja Nyboe.     *
  * ------------------------------------------------------------------------ *
- *                           Special clan module                            *
+ *                                Planets                                   *
  ****************************************************************************/
 
 #include <ctype.h>
