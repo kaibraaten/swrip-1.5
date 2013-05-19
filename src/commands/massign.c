fa33232d17a15988c267479d9e86816df883cb5b
Kai Braaten <kai.braaten@gmail.com>
2013-05-19 03:29:18 +0000

    Code for massign had gone, uhm, "missing". Restored from earlier revision.

--- src/commands/massign.c
+++ src/commands/massign.c
@@ -1,1 +1,42 @@
 #include "mud.h"
+
+void do_massign( CHAR_DATA *ch, char *argument )
+{
+  char arg1[MAX_INPUT_LENGTH];
+  char arg2[MAX_INPUT_LENGTH];
+  char arg3[MAX_INPUT_LENGTH];
+  short  m_lo, m_hi;
+  CHAR_DATA *victim;
+
+  argument = one_argument( argument, arg1 );
+  argument = one_argument( argument, arg2 );
+  argument = one_argument( argument, arg3 );
+  m_lo = atoi( arg2 );  m_hi = atoi( arg3 );
+
+  if ( arg1[0] == '\0' || m_lo < 0 || m_hi < 0 )
+    {
+      send_to_char( "Syntax: massign <who> <low> <high>\r\n", ch );
+      return;
+    }
+  if ( (victim = get_char_world( ch, arg1 )) == NULL )
+    {
+      send_to_char( "They don't seem to be around.\r\n", ch );
+      return;
+    }
+  if ( IS_NPC( victim ) || get_trust( victim ) < LEVEL_SAVIOR )
+    {
+      send_to_char( "They wouldn't know what to do with a monster range.\r\n", ch );
+      return;
+    }
+  if ( m_lo > m_hi )
+    {
+      send_to_char( "Unacceptable monster range.\r\n", ch );
+      return;
+    }
+  victim->pcdata->m_range_lo = m_lo;
+  victim->pcdata->m_range_hi = m_hi;
+  assign_area( victim );
+  send_to_char( "Done.\r\n", ch );
+  ch_printf( victim, "%s has assigned you the monster vnum range %d - %d.\r\n",
+             ch->name, m_lo, m_hi );
+}
