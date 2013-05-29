917935a86d9d2aa59e4a57a41031825b7040fcc8
Kai Braaten <kai.braaten@gmail.com>
2013-05-29 09:22:45 +0000

    sdf

--- src/commands/hedit.c
+++ src/commands/hedit.c
@@ -8,7 +8,7 @@
  */
 void do_hedit( CHAR_DATA *ch, char *argument )
 {
-  HELP_DATA *pHelp;
+  HELP_DATA *pHelp = NULL;
 
   if ( !ch->desc )
     {
@@ -22,7 +22,9 @@ void do_hedit( CHAR_DATA *ch, char *argument )
       break;
 
     case SUB_HELP_EDIT:
-      if ( (pHelp = (HELP_DATA*)ch->dest_buf) == NULL )
+      pHelp = (HELP_DATA*) ch->dest_buf;
+
+      if ( !pHelp )
         {
           bug( "hedit: sub_help_edit: NULL ch->dest_buf", 0 );
           stop_editing( ch );
@@ -35,7 +37,9 @@ void do_hedit( CHAR_DATA *ch, char *argument )
       return;
     }
 
-  if ( (pHelp = get_help( ch, argument )) == NULL )     /* new help */
+  pHelp = get_help( ch, argument );
+
+  if ( !pHelp ) /* new help */
     {
       int level = get_trust( ch );
       pHelp = create_help( argument, level );
--- src/commands/help.c
+++ src/commands/help.c
@@ -8,7 +8,8 @@ static void similar_help_files(CHAR_DATA *ch, char *argument);
 
 void do_help( CHAR_DATA *ch, char *argument )
 {
-  HELP_DATA *pHelp;
+  HELP_DATA *pHelp = NULL;
+  const char *help_text = NULL;
 
   if ( !argument || argument[0] == '\0')
     {
@@ -16,7 +17,9 @@ void do_help( CHAR_DATA *ch, char *argument )
       return;
     }
 
-  if ( (pHelp = get_help( ch, argument )) == NULL )
+  pHelp = get_help( ch, argument );
+
+  if ( !pHelp )
     {
       pager_printf( ch, "&C&wNo help on \'%s\' found.\r\n", argument );
       similar_help_files(ch, argument);
@@ -25,20 +28,27 @@ void do_help( CHAR_DATA *ch, char *argument )
 
   if ( pHelp->level >= 0 && str_cmp( argument, "imotd" ) )
     {
-      send_to_pager( pHelp->keyword, ch );
-      send_to_pager( "\r\n", ch );
+      pager_printf( ch, "%s\r\n", pHelp->keyword );
     }
 
   if ( !is_npc(ch) && IS_SET( ch->act , PLR_SOUND ) )
-    send_to_pager( "!!SOUND(help)", ch );
+    {
+      pager_printf( ch, "!!SOUND(help)" );
+    }
 
   /*
    * Strip leading '.' to allow initial blanks.
    */
   if ( pHelp->text[0] == '.' )
-    send_to_pager( pHelp->text+1, ch );
+    {
+      help_text = pHelp->text + 1;
+    }
   else
-    send_to_pager( pHelp->text  , ch );
+    {
+      help_text = pHelp->text;
+    }
+
+  pager_printf( ch, help_text );
 }
 
 static short str_similarity( const char *astr, const char *bstr )
@@ -62,66 +72,65 @@ static short str_similarity( const char *astr, const char *bstr )
 
 static void similar_help_files(CHAR_DATA *ch, char *argument)
 {
-  HELP_DATA *pHelp=NULL;
-  char buf[MAX_STRING_LENGTH];
-  char *extension;
-  short lvl=0;
-  bool single=FALSE;
+  HELP_DATA *pHelp = NULL;
+  short level = 0;
+  bool single = FALSE;
 
-  send_to_pager( "&C&BSimilar Help Files:\r\n", ch);
+  pager_printf( ch, "&C&BSimilar Help Files:\r\n" );
 
   for ( pHelp = first_help; pHelp; pHelp=pHelp->next)
     {
-      buf[0]='\0';
-      extension=pHelp->keyword;
+      char buf[MAX_STRING_LENGTH];
+      char *extension = pHelp->keyword;
 
       if (pHelp->level > get_trust(ch))
-        continue;
+	{
+	  continue;
+	}
 
       while ( extension[0] != '\0' )
         {
           extension= one_argument(extension, buf);
 
-          if ( str_similarity(argument, buf) > lvl)
+          if ( str_similarity(argument, buf) > level)
             {
-              lvl=str_similarity(argument, buf);
-	      single=TRUE;
+              level = str_similarity(argument, buf);
+	      single = TRUE;
             }
-          else if ( str_similarity(argument, buf) == lvl && lvl > 0)
+          else if ( str_similarity(argument, buf) == level && level > 0)
             {
-              single=FALSE;
+              single = FALSE;
             }
         }
     }
 
-  if (lvl==0)
+  if (level == 0)
     {
-      send_to_pager( "&C&GNo similar help files.\r\n", ch);
+      pager_printf( ch, "&C&GNo similar help files.\r\n" );
       return;
     }
 
   for ( pHelp = first_help; pHelp; pHelp=pHelp->next)
     {
-      buf[0]='\0';
-      extension=pHelp->keyword;
+      char buf[MAX_STRING_LENGTH];
+      char *extension = pHelp->keyword;
 
       while ( extension[0] != '\0' )
         {
           extension=one_argument(extension, buf);
 
-          if ( str_similarity(argument, buf) >= lvl
+          if ( str_similarity(argument, buf) >= level
                && pHelp->level <= get_trust(ch))
             {
               if (single)
                 {
-                  send_to_pager( "&C&GOpening only similar helpfile.&C\r\n", ch);
+                  pager_printf( ch, "&C&GOpening only similar helpfile.&C\r\n" );
                   do_help( ch, buf);
                   return;
                 }
 
               pager_printf(ch, "&C&G   %s\r\n", pHelp->keyword);
               break;
-
             }
         }
     }
