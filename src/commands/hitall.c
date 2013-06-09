8da12584c874d2c3eb663f7f0ebfb8969dde2083
Kai Braaten <kai.braaten@gmail.com>
2013-06-09 16:31:08 +0000

    sdf

--- src/commands/hitall.c
+++ src/commands/hitall.c
@@ -5,8 +5,8 @@ static bool is_legal_kill(Character *ch, Character *vch);
 
 void do_hitall( Character *ch, char *argument )
 {
-  Character *vch;
-  Character *vch_next;
+  Character *vch = NULL;
+  Character *vch_next = NULL;
   short nvict = 0;
   short nhit = 0;
   short percent;
@@ -49,30 +49,43 @@ void do_hitall( Character *ch, char *argument )
           global_retcode = one_hit(ch, vch, TYPE_UNDEFINED);
         }
       else
-        global_retcode = damage(ch, vch, 0, TYPE_UNDEFINED);
+	{
+	  global_retcode = damage(ch, vch, 0, TYPE_UNDEFINED);
+	}
+
       /* Fireshield, etc. could kill ch too.. :>.. -- Altrag */
       if ( global_retcode == rCHAR_DIED || global_retcode == rBOTH_DIED
            ||   char_died(ch) )
-        return;
+	{
+	  return;
+	}
     }
+
   if ( !nvict )
     {
       send_to_char( "There's no one here!\r\n", ch );
       return;
     }
+
   ch->move = UMAX(0, ch->move-nvict*3+nhit);
 
   if ( nhit )
-    learn_from_success(ch, gsn_hitall);
+    {
+      learn_from_success(ch, gsn_hitall);
+    }
   else
-    learn_from_failure(ch, gsn_hitall);
+    {
+      learn_from_failure(ch, gsn_hitall);
+    }
 }
 
 static bool is_legal_kill(Character *ch, Character *vch)
 {
   if ( is_npc(ch) || is_npc(vch) )
     return TRUE;
+
   if ( is_safe(ch,vch) )
     return FALSE;
+
   return TRUE;
 }
