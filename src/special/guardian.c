5b459acd79f3906f93a1d4f61be7a82da1264821
Kai Braaten <kai.braaten@gmail.com>
2013-06-12 23:59:51 +0000

    sdf

--- src/special/guardian.c
+++ src/special/guardian.c
@@ -3,48 +3,29 @@
 
 bool spec_guardian( Character *ch )
 {
-  char buf[MAX_STRING_LENGTH];
-  Character *victim;
-  Character *v_next;
-  Character *ech;
-  char *crime;
-  int max_evil;
-
+  CerisListIterator *iter = NULL;
+  Character *ech = NULL;
+  int max_evil = 300;
+  
   if ( !is_awake(ch) || ch->fighting )
     return FALSE;
 
-  max_evil = 300;
-  ech      = NULL;
-  crime    = "";
+  iter = CreateListIterator( ch->in_room->People, ForwardsIterator );
 
-  for ( victim = ch->in_room->first_person; victim; victim = v_next )
+  for( ; !ListIterator_IsDone( iter ); ListIterator_Next( iter ) )
     {
-      v_next = victim->next_in_room;
+      Character *victim = (Character*) ListIterator_GetData( iter );
+
       if ( victim->fighting
-           &&   who_fighting( victim ) != ch
-           &&   victim->alignment < max_evil )
+           && who_fighting( victim ) != ch
+           && victim->alignment < max_evil )
         {
           max_evil = victim->alignment;
           ech      = victim;
         }
     }
 
-  if ( victim && IS_SET( ch->in_room->room_flags, ROOM_SAFE ) )
-    {
-      sprintf( buf, "%s is a %s!  As well as a COWARD!",
-               victim->name, crime );
-      do_yell( ch, buf );
-      return TRUE;
-    }
-
-  if ( victim )
-    {
-      sprintf( buf, "%s is a %s!  PROTECT THE INNOCENT!!",
-               victim->name, crime );
-      do_shout( ch, buf );
-      multi_hit( ch, victim, TYPE_UNDEFINED );
-      return TRUE;
-    }
+  DestroyListIterator( iter );
 
   if ( ech )
     {
