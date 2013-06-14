6ce1960bea4c168f1ef25efdc5c1dde31b801c09
Kai Braaten <kai.braaten@gmail.com>
2013-06-14 08:46:13 +0000

    sdf

--- src/spells/faeriefog.c
+++ src/spells/faeriefog.c
@@ -1,31 +1,60 @@
 #include "character.h"
 #include "mud.h"
 
-ch_ret spell_faerie_fog( int sn, int level, Character *ch, void *vo )
+typedef struct CastingInfo
 {
-  Character *ich;
+  const Character *Caster;
+  int CastingLevel;
+} CastingInfo;
 
-  act( AT_MAGIC, "$n conjures a cloud of purple smoke.", ch, NULL, NULL, TO_ROOM );
-  act( AT_MAGIC, "You conjure a cloud of purple smoke.", ch, NULL, NULL, TO_CHAR );
+static void MakeVisible( void *element, void *userData )
+{
+  Character *victim = (Character*) element;
+  const CastingInfo *castingInfo = (CastingInfo*) userData;
+  const Character *caster = castingInfo->Caster;
+  int level = castingInfo->CastingLevel;
 
-  for ( ich = ch->in_room->first_person; ich; ich = ich->next_in_room )
+  if ( !IsNpc(victim) && IS_SET(victim->act, PLR_WIZINVIS) )
     {
-      if ( !IsNpc(ich) && IS_SET(ich->act, PLR_WIZINVIS) )
-        continue;
-
-      if ( ich == ch || saves_spell_staff( level, ich ) )
-        continue;
-
-      affect_strip ( ich, gsn_invis                     );
-      affect_strip ( ich, gsn_mass_invis                );
-      affect_strip ( ich, gsn_sneak                     );
-      if ( ich->race != RACE_DEFEL )
-        REMOVE_BIT   ( ich->affected_by, AFF_HIDE       );
-      REMOVE_BIT   ( ich->affected_by, AFF_INVISIBLE    );
-      if ( !permsneak(ich) )
-        REMOVE_BIT   ( ich->affected_by, AFF_SNEAK      );
-      act( AT_MAGIC, "$n is revealed!", ich, NULL, NULL, TO_ROOM );
-      act( AT_MAGIC, "You are revealed!", ich, NULL, NULL, TO_CHAR );
+      return;
     }
+
+  if ( victim == caster || saves_spell_staff( level, victim ) )
+    {
+      return;
+    }
+
+  affect_strip( victim, gsn_invis );
+  affect_strip( victim, gsn_mass_invis );
+  affect_strip( victim, gsn_sneak );
+
+  if ( victim->race != RACE_DEFEL )
+    {
+      REMOVE_BIT( victim->affected_by, AFF_HIDE );
+    }
+
+  REMOVE_BIT( victim->affected_by, AFF_INVISIBLE );
+
+  if ( !permsneak(victim) )
+    {
+      REMOVE_BIT( victim->affected_by, AFF_SNEAK );
+    }
+
+  act( AT_MAGIC, "$n is revealed!", victim, NULL, NULL, TO_ROOM );
+  act( AT_MAGIC, "You are revealed!", victim, NULL, NULL, TO_CHAR );
+}
+
+ch_ret spell_faerie_fog( int sn, int level, Character *caster, void *vo )
+{
+  CastingInfo castingInfo;
+
+  castingInfo.Caster = caster;
+  castingInfo.CastingLevel = level;
+
+  act( AT_MAGIC, "$n conjures a cloud of purple smoke.", caster, NULL, NULL, TO_ROOM );
+  act( AT_MAGIC, "You conjure a cloud of purple smoke.", caster, NULL, NULL, TO_CHAR );
+
+  List_ForEach( caster->in_room->People, MakeVisible, &castingInfo );
+
   return rNONE;
 }
