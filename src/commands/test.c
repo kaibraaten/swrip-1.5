258b0adea6b965414400dc19c79c021c2189d5b4
Kai Braaten <kai.braaten@gmail.com>
2014-07-15 13:01:57 +0000

    New option to TEST command to find skills without teachers.

--- src/commands/test.c
+++ src/commands/test.c
@@ -8,6 +8,8 @@ static char is_in_list( int needle, const int *haystack, size_t list_size );
 static size_t get_list_size( const int *teachers );
 static int comparator( const void*, const void* );
 static AREA_DATA *get_area( vnum_t vnum );
+static void command_teachers( CHAR_DATA *ch );
+static void command_noteacher( CHAR_DATA *ch );
 
 #define MAX_TEACHER 10000
 
@@ -16,55 +18,85 @@ void do_test( CHAR_DATA *ch, char *argument )
   char command[MAX_INPUT_LENGTH];
   argument = one_argument( argument, command );
 
-  if( !str_cmp(command, "teachers") )
+  if( !str_cmp( command, "teachers" ) )
     {
-      int sn = 0;
-      int i = 0;
-      int teachers[MAX_TEACHER];
-      size_t list_size = 0;
-      const AREA_DATA *area = NULL;
+      command_teachers( ch );
+      return;
+    }
 
-      memset( teachers, 0, sizeof( teachers ) );
-
-      for( sn = 0; sn < top_sn; sn++ )
-	{
-	  const SKILLTYPE *skill = skill_table[sn];
-
-	  if( skill->teachers )
-	    {
-	      store_teachers( teachers, skill );
-	    }
-	}
-
-      list_size = get_list_size( teachers );
-
-      qsort( teachers, list_size, sizeof( int ), comparator );
-
-      for( i = 0; i < MAX_TEACHER; i++ )
-	{
-	  vnum_t vnum = teachers[i];
-
-	  if( vnum == 0 )
-	    {
-	      break;
-	    }
-
-	  area = get_area( vnum );
-
-	  if( area )
-	    {
-	      ch_printf( ch, "&w%s\r\n", area->filename );
-	    }
-	  else
-	    {
-	      ch_printf( ch, "&r%d doesn't exist&w\r\n", vnum );
-	    }
-	}
-
+  if( !str_cmp( command, "noteacher" ) )
+    {
+      command_noteacher(ch);
       return;
     }
 
   ch_printf(ch, "Unknown option.\r\n");
+}
+
+static void command_noteacher( CHAR_DATA *ch )
+{
+  int sn = 0;
+
+  for( sn = 0; sn < top_sn; sn++ )
+    {
+      const SKILLTYPE *skill = skill_table[sn];
+
+      if( skill->type == SKILL_TONGUE )
+	{
+	  continue;
+	}
+
+      if( !skill->teachers || strlen( skill->teachers ) == 0 )
+        {
+	  ch_printf(ch, "%s has no teachers\r\n", skill->name);
+        }
+    }
+}
+
+static void command_teachers( CHAR_DATA *ch )
+{
+  int sn = 0;
+  int i = 0;
+  int teachers[MAX_TEACHER];
+  size_t list_size = 0;
+  const AREA_DATA *area = NULL;
+
+  memset( teachers, 0, sizeof( teachers ) );
+
+  for( sn = 0; sn < top_sn; sn++ )
+    {
+      const SKILLTYPE *skill = skill_table[sn];
+
+      if( skill->teachers )
+	{
+	  store_teachers( teachers, skill );
+	}
+    }
+
+  list_size = get_list_size( teachers );
+
+  qsort( teachers, list_size, sizeof( int ), comparator );
+
+  for( i = 0; i < MAX_TEACHER; i++ )
+    {
+      vnum_t vnum = teachers[i];
+
+      if( vnum == 0 )
+	{
+	  break;
+	}
+
+      area = get_area( vnum );
+
+      if( area )
+	{
+	  ch_printf( ch, "&w%s\r\n", area->filename );
+	}
+      else
+	{
+	  ch_printf( ch, "&r%d doesn't exist&w\r\n", vnum );
+	}
+    }
 }
 
 static int comparator( const void *a, const void *b )
