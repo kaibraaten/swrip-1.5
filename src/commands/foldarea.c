3cb5a03102ad4acfb3e046684ac5f4a2d4845241
Kai Braaten <kai.braaten@gmail.com>
2014-07-23 22:17:19 +0000

    sdf

--- src/commands/foldarea.c
+++ src/commands/foldarea.c
@@ -4,6 +4,8 @@ void do_foldarea( CHAR_DATA *ch, char *argument )
 {
   AREA_DATA *tarea = NULL;
   char arg[MAX_INPUT_LENGTH];
+  bool fold_all_areas = false;
+  bool found = false;
 
   argument = one_argument( argument, arg );
 
@@ -13,21 +15,27 @@ void do_foldarea( CHAR_DATA *ch, char *argument )
       return;
     }
 
+  fold_all_areas = !str_cmp( arg, "all" );
+
   for ( tarea = first_area; tarea; tarea = tarea->next )
     {
-      if ( !str_cmp( tarea->filename, arg ) )
+      if ( !str_cmp( tarea->filename, arg ) || fold_all_areas )
         {
-          send_to_char( "Folding...\r\n", ch );
-
           if (!str_cmp( argument, "remproto") )
             fold_area( tarea, tarea->filename, TRUE );
           else
             fold_area( tarea, tarea->filename, FALSE );
 
-          send_to_char( "Done.\r\n", ch );
-          return;
+	  found = true;
         }
     }
 
-  send_to_char( "No such area exists.\r\n", ch );
+  if( found )
+    {
+      ch_printf( ch, "Done.\r\n" );
+    }
+  else
+    {
+      ch_printf( ch, "No such area exists.\r\n" );
+    }
 }
