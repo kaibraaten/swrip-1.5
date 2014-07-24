7f6ebeb8259b89184445a99de30769c3e4609b7c
Kai Braaten <kai.braaten@gmail.com>
2014-07-24 12:21:17 +0000

    sdf

--- src/commands/makegrenade.c
+++ src/commands/makegrenade.c
@@ -2,113 +2,141 @@
 #include "mud.h"
 #include "character.h"
 
+static void on_start( CHAR_DATA *ch, char *argument );
+static void on_finished( CHAR_DATA *ch );
+static void on_abort( CHAR_DATA *ch );
+
 void do_makegrenade( CHAR_DATA *ch, char *argument )
 {
-  char arg[MAX_INPUT_LENGTH];
-  char buf[MAX_STRING_LENGTH];
-  int level, the_chance, weight = 0, strength = 0;
-  bool checktool, checkdrink, checkbatt, checkchem, checkcirc;
-  OBJ_DATA *obj = NULL;
-  OBJ_INDEX_DATA *pObjIndex = NULL;
-  int vnum = 0;
-
-  strcpy( arg , argument );
-
   switch( ch->substate )
     {
     default:
-      if ( arg[0] == '\0' )
-        {
-          send_to_char( "&RUsage: Makegrenade <name>\r\n&w", ch);
-          return;
-        }
-
-      checktool  = FALSE;
-      checkdrink = FALSE;
-      checkbatt  = FALSE;
-      checkchem  = FALSE;
-      checkcirc  = FALSE;
-
-      if ( !IS_SET( ch->in_room->room_flags, ROOM_FACTORY ) )
-        {
-          send_to_char( "&RYou need to be in a factory or workshop to do that.\r\n", ch);
-          return;
-        }
-
-      for ( obj = ch->last_carrying; obj; obj = obj->prev_content )
-        {
-          if (obj->item_type == ITEM_TOOLKIT)
-            checktool = TRUE;
-          if (obj->item_type == ITEM_DRINK_CON && obj->value[1] == 0 )
-            checkdrink = TRUE;
-          if (obj->item_type == ITEM_BATTERY)
-            checkbatt = TRUE;
-          if (obj->item_type == ITEM_CIRCUIT)
-            checkcirc = TRUE;
-          if (obj->item_type == ITEM_CHEMICAL)
-            checkchem = TRUE;
-        }
-
-      if ( !checktool )
-        {
-          send_to_char( "&RYou need toolkit to make a grenade.\r\n", ch);
-          return;
-        }
-
-      if ( !checkdrink )
-        {
-          send_to_char( "&RYou will need an empty drink container to mix and hold the chemicals.\r\n", ch);
-          return;
-        }
-
-      if ( !checkbatt )
-        {
-          send_to_char( "&RYou need a small battery for the timer.\r\n", ch);
-          return;
-        }
-
-      if ( !checkcirc )
-        {
-          send_to_char( "&RYou need a small circuit for the timer.\r\n", ch);
-          return;
-	}
-
-      if ( !checkchem )
-        {
-          send_to_char( "&RSome explosive chemicals would come in handy!\r\n", ch);
-          return;
-        }
-
-      the_chance = is_npc(ch) ? ch->top_level
-        : (int) (ch->pcdata->learned[gsn_makegrenade]);
-      if ( number_percent( ) < the_chance )
-        {
-          send_to_char( "&GYou begin the long process of making a grenade.\r\n", ch);
-          act( AT_PLAIN, "$n takes $s tools and a drink container and begins to work on something.", ch,
-               NULL, argument , TO_ROOM );
-          add_timer ( ch , TIMER_DO_FUN , 25 , do_makegrenade , 1 );
-          ch->dest_buf   = str_dup(arg);
-          return;
-        }
-      send_to_char("&RYou can't figure out how to fit the parts together.\r\n",ch);
-      learn_from_failure( ch, gsn_makegrenade );
-      return;
+      on_start( ch, argument );
+      break;
 
     case SUB_PAUSE:
-      if ( !ch->dest_buf )
-        return;
-      strcpy(arg, (const char*)ch->dest_buf);
-      DISPOSE( ch->dest_buf);
+      ch->substate = SUB_NONE;
+      on_finished( ch );
       break;
 
     case SUB_TIMER_DO_ABORT:
-      DISPOSE( ch->dest_buf );
       ch->substate = SUB_NONE;
-      send_to_char("&RYou are interupted and fail to finish your work.\r\n", ch);
+      on_abort( ch );
+      break;
+    }
+}
+
+static void on_start( CHAR_DATA *ch, char *argument )
+{
+  char arg[MAX_INPUT_LENGTH];
+  int the_chance = 0;
+  bool checktool = false;
+  bool checkdrink = false;
+  bool checkbatt = false;
+  bool checkchem = false;
+  bool checkcirc = false;
+  OBJ_DATA *obj = NULL;
+
+  strcpy( arg , argument );
+
+  if ( arg[0] == '\0' )
+    {
+      send_to_char( "&RUsage: Makegrenade <name>\r\n&w", ch);
       return;
     }
 
-  ch->substate = SUB_NONE;
+  if ( !IS_SET( ch->in_room->room_flags, ROOM_FACTORY ) )
+    {
+      send_to_char( "&RYou need to be in a factory or workshop to do that.\r\n", ch);
+      return;
+    }
+
+  for ( obj = ch->last_carrying; obj; obj = obj->prev_content )
+    {
+      if (obj->item_type == ITEM_TOOLKIT)
+	checktool = true;
+      if (obj->item_type == ITEM_DRINK_CON && obj->value[1] == 0 )
+	checkdrink = true;
+      if (obj->item_type == ITEM_BATTERY)
+	checkbatt = true;
+      if (obj->item_type == ITEM_CIRCUIT)
+	checkcirc = true;
+      if (obj->item_type == ITEM_CHEMICAL)
+	checkchem = true;
+    }
+
+  if ( !checktool )
+    {
+      send_to_char( "&RYou need toolkit to make a grenade.\r\n", ch);
+      return;
+    }
+
+  if ( !checkdrink )
+    {
+      send_to_char( "&RYou will need an empty drink container to mix and hold the chemicals.\r\n", ch);
+      return;
+    }
+
+  if ( !checkbatt )
+    {
+      send_to_char( "&RYou need a small battery for the timer.\r\n", ch);
+      return;
+    }
+
+  if ( !checkcirc )
+    {
+      send_to_char( "&RYou need a small circuit for the timer.\r\n", ch);
+      return;
+    }
+
+  if ( !checkchem )
+    {
+      send_to_char( "&RSome explosive chemicals would come in handy!\r\n", ch);
+      return;
+    }
+
+  the_chance = is_npc(ch) ? ch->top_level
+    : (int) (ch->pcdata->learned[gsn_makegrenade]);
+
+  if ( number_percent( ) < the_chance )
+    {
+      send_to_char( "&GYou begin the long process of making a grenade.\r\n", ch);
+      act( AT_PLAIN, "$n takes $s tools and a drink container and begins to work on something.", ch,
+	   NULL, NULL , TO_ROOM );
+      add_timer ( ch , TIMER_DO_FUN , 25 , do_makegrenade , 1 );
+      ch->dest_buf   = str_dup(arg);
+      return;
+    }
+  else
+    {
+      send_to_char("&RYou can't figure out how to fit the parts together.\r\n",ch);
+      learn_from_failure( ch, gsn_makegrenade );
+    }
+}
+
+static void on_finished( CHAR_DATA *ch )
+{
+  char arg[MAX_INPUT_LENGTH];
+  char buf[MAX_STRING_LENGTH];
+  int level = 0;
+  int the_chance = 0;
+  int weight = 0;
+  int strength = 0;
+  bool checktool = false;
+  bool checkdrink = false;
+  bool checkbatt = false;
+  bool checkchem = false;
+  bool checkcirc = false;
+  OBJ_DATA *obj = NULL;
+  OBJ_INDEX_DATA *pObjIndex = NULL;
+  vnum_t vnum = INVALID_VNUM;
+  long xpgain = 0;
+
+  if ( !ch->dest_buf )
+    return;
+
+  strcpy(arg, (const char*)ch->dest_buf);
+  DISPOSE( ch->dest_buf);
 
   level = is_npc(ch) ? ch->top_level : (int) (ch->pcdata->learned[gsn_makegrenade]);
   vnum = OBJ_VNUM_CRAFTING_GRENADE;
@@ -119,29 +147,23 @@ void do_makegrenade( CHAR_DATA *ch, char *argument )
       return;
     }
 
-  checktool = FALSE;
-  checkdrink = FALSE;
-  checkbatt = FALSE;
-  checkchem = FALSE;
-  checkcirc = FALSE;
-
   for ( obj = ch->last_carrying; obj; obj = obj->prev_content )
     {
       if (obj->item_type == ITEM_TOOLKIT)
-        checktool = TRUE;
-      if (obj->item_type == ITEM_DRINK_CON && checkdrink == FALSE && obj->value[1] == 0 )
+        checktool = true;
+      if (obj->item_type == ITEM_DRINK_CON && checkdrink == false && obj->value[1] == 0 )
         {
-          checkdrink = TRUE;
+          checkdrink = true;
           separate_obj( obj );
           obj_from_char( obj );
           extract_obj( obj );
         }
-      if (obj->item_type == ITEM_BATTERY && checkbatt == FALSE)
+      if (obj->item_type == ITEM_BATTERY && checkbatt == false)
         {
           separate_obj( obj );
           obj_from_char( obj );
           extract_obj( obj );
-          checkbatt = TRUE;
+          checkbatt = true;
         }
       if (obj->item_type == ITEM_CHEMICAL)
         {
@@ -150,14 +172,14 @@ void do_makegrenade( CHAR_DATA *ch, char *argument )
           separate_obj( obj );
           obj_from_char( obj );
           extract_obj( obj );
-          checkchem = TRUE;
+          checkchem = true;
         }
-      if (obj->item_type == ITEM_CIRCUIT && checkcirc == FALSE)
+      if (obj->item_type == ITEM_CIRCUIT && checkcirc == false)
         {
           separate_obj( obj );
           obj_from_char( obj );
           extract_obj( obj );
-          checkcirc = TRUE;
+          checkcirc = true;
         }
     }
 
@@ -196,14 +218,17 @@ void do_makegrenade( CHAR_DATA *ch, char *argument )
 
   send_to_char( "&GYou finish your work and hold up your newly created grenade.&w\r\n", ch);
   act( AT_PLAIN, "$n finishes making $s new grenade.", ch,
-       NULL, argument , TO_ROOM );
+       NULL, NULL , TO_ROOM );
 
-  {
-    long xpgain;
+  xpgain = UMIN( obj->cost*50 ,( exp_level(get_level( ch, ENGINEERING_ABILITY ) + 1) - exp_level(get_level( ch, ENGINEERING_ABILITY ) ) ) );
+  gain_exp(ch, ENGINEERING_ABILITY, xpgain );
+  ch_printf( ch , "You gain %d engineering experience.", xpgain );
 
-    xpgain = UMIN( obj->cost*50 ,( exp_level(get_level( ch, ENGINEERING_ABILITY ) + 1) - exp_level(get_level( ch, ENGINEERING_ABILITY ) ) ) );
-    gain_exp(ch, ENGINEERING_ABILITY, xpgain );
-    ch_printf( ch , "You gain %d engineering experience.", xpgain );
-  }
   learn_from_success( ch, gsn_makegrenade );
+}
+
+static void on_abort( CHAR_DATA *ch )
+{
+  DISPOSE( ch->dest_buf );
+  send_to_char("&RYou are interupted and fail to finish your work.\r\n", ch);
 }
