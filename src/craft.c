2a674e412b809846dbd5cbabb9d8b0094cee341e
Kai Braaten <kai.braaten@gmail.com>
2014-07-29 14:20:12 +0000

    sdf

--- src/craft.c
+++ src/craft.c
@@ -1,8 +1,12 @@
+#include "mud.h"
 #include "craft.h"
+#include "constants.h"
 
 struct Craft
 {
-  int Foo;
+  int XpAbility;
+  int Skill;
+  Character *Engineer;
 };
 
 Craft *AllocateCraft( void )
@@ -11,10 +15,38 @@ Craft *AllocateCraft( void )
 
   CREATE( craft, Craft, 1 );
 
+  craft->XpAbility = ABILITY_NONE;
+  craft->Skill = 0;
+  craft->Engineer = NULL;
+
   return craft;
 }
 
 void FreeCraft( Craft *craft )
 {
+  DISPOSE( craft );
+}
 
+void SetCraftXpAbility( Craft *craft, int ability )
+{
+  if( ability > ABILITY_NONE && ability < MAX_ABILITY )
+    {
+      craft->XpAbility = ability;
+    }
+  else
+    {
+      bug( "%s/%s/%d: invalid ability %d", __FILE__, __FUNCTION__, __LINE__, ability );
+    }
+}
+
+void SetCraftSkill( Craft *craft, int sn )
+{
+  if( get_skilltype( sn ) )
+    {
+      craft->Skill = sn;
+    }
+  else
+    {
+      bug( "%s/%s/%d: invalid skill number %d", __FILE__, __FUNCTION__, __LINE__, sn );
+    }
 }
