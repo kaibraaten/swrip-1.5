5d240b3f5482612bd78bffdbe2c28e330fffebb6
Kai Braaten <kai.braaten@gmail.com>
2014-08-13 23:24:46 +0000

    sdf

--- src/commands/makecomlink.c
+++ src/commands/makecomlink.c
@@ -1,254 +1,130 @@
 #include <string.h>
 #include "mud.h"
 #include "character.h"
+#include "craft.h"
 
-static void OnStart( Character *ch, char *argument );
-static void OnFinished( Character *ch );
-static void OnAbort( Character *ch );
+enum { WearLocation, ItemName };
+
+static void InterpretArgumentsHandler( void *userData, InterpretArgumentsEventArgs *args );
+static void SetObjectStatsHandler( void *userData, SetObjectStatsEventArgs *args );
+static CraftRecipe *MakeCraftRecipe( void );
 
 void do_makecomlink( Character *ch, char *argument )
 {
-  switch( ch->substate )
-    {
-    default:
-      OnStart( ch, argument );
-      break;
+  CraftRecipe *recipe = MakeCraftRecipe();
+  CraftingSession *session = AllocateCraftingSession( recipe, ch, argument );
 
-    case SUB_PAUSE:
-      ch->substate = SUB_NONE;
-      OnFinished( ch );
-      break;
+  AddInterpretArgumentsCraftingHandler( session, NULL, InterpretArgumentsHandler );
+  AddSetObjectStatsCraftingHandler( session, NULL, SetObjectStatsHandler );
 
-    case SUB_TIMER_DO_ABORT:
-      ch->substate = SUB_NONE;
-      OnAbort( ch );
-      break;
-    }
+  StartCrafting( session );
 }
 
-static void OnStart( Character *ch, char *argument )
+static CraftRecipe *MakeCraftRecipe( void )
 {
-  char arg[MAX_INPUT_LENGTH];
-  char arg2[MAX_INPUT_LENGTH];
-  int the_chance = 0;
-  bool checktool = false;
-  bool checkgem = false;
-  bool checkbatt = false;
-  bool checkcirc = false;
-  OBJ_DATA *obj = NULL;
-
-  argument = one_argument( argument, arg );
-  strcpy ( arg2, argument);
-
-  if ( arg2[0] == '\0' )
+  static const struct CraftingMaterial materials[] =
     {
-      send_to_char( "&RUsage: Makecomlink <wearloc> <name>\r\n&w", ch);
-      return;
-    }
+      { ITEM_TOOLKIT,  CRAFTFLAG_NONE },
+      { ITEM_OVEN,     CRAFTFLAG_NONE },
+      { ITEM_CIRCUIT,  CRAFTFLAG_EXTRACT },
+      { ITEM_CRYSTAL,  CRAFTFLAG_EXTRACT },
+      { ITEM_NONE,     CRAFTFLAG_NONE }
+    };
+  CraftRecipe *recipe = AllocateCraftRecipe( gsn_makecomlink, materials,
+					     10, OBJ_VNUM_CRAFTING_COMLINK,
+					     CRAFTFLAG_NEED_WORKSHOP );
 
-  if ( !str_cmp( arg, "body" )
-       || !str_cmp( arg, "head" )
-       || !str_cmp( arg, "legs" )
-       || !str_cmp( arg, "arms" )
-       || !str_cmp( arg, "about" )
-       || !str_cmp( arg, "eyes" )
-       || !str_cmp( arg, "waist" )
-       || !str_cmp( arg, "hold" )
-       || !str_cmp( arg, "feet" )
-       || !str_cmp( arg, "hands" ) )
-    {
-      send_to_char( "&RYou cannot make a comlink for that body part.\r\n&w", ch);
-      return;
-    }
-
-  if ( !str_cmp( arg, "shield" ) )
-    {
-      send_to_char( "&RYou cannot make a comlink worn as a shield.\r\n&w", ch);
-      send_to_char( "&RTry MAKESHIELD.\r\n&w", ch);
-      return;
-    }
-
-  if ( !str_cmp( arg, "wield" ) )
-    {
-      send_to_char( "&RAre you going to fight with your comlink?\r\n&w", ch);
-      return;
-    }
-
-  if ( !IS_SET( ch->in_room->room_flags, ROOM_FACTORY ) )
-    {
-      send_to_char( "&RYou need to be in a factory or workshop to do that.\r\n", ch);
-      return;
-    }
-
-  for ( obj = ch->last_carrying; obj; obj = obj->prev_content )
-    {
-      if (obj->item_type == ITEM_TOOLKIT)
-	checktool = true;
-      if (obj->item_type == ITEM_CRYSTAL)
-	checkgem = true;
-      if (obj->item_type == ITEM_BATTERY)
-	checkbatt = true;
-      if (obj->item_type == ITEM_CIRCUIT)
-	checkcirc = true;
-    }
-
-  if ( !checktool )
-    {
-      send_to_char( "&RYou need toolkit to make a comlink.\r\n", ch);
-      return;
-    }
-
-  if ( !checkgem )
-    {
-      send_to_char( "&RYou need a small crystal.\r\n", ch);
-      return;
-    }
-
-  if ( !checkbatt )
-    {
-      send_to_char( "&RYou need a power source for your comlink.\r\n", ch);
-      return;
-    }
-
-  if ( !checkcirc )
-    {
-      send_to_char( "&RYou need a small circuit.\r\n", ch);
-      return;
-    }
-
-  the_chance = is_npc(ch) ? ch->top_level
-    : (int) (ch->pcdata->learned[gsn_makecomlink]);
-
-  if ( number_percent( ) < the_chance )
-    {
-      send_to_char( "&GYou begin the long process of making a comlink.\r\n", ch);
-      act( AT_PLAIN, "$n takes $s tools and begins to work on something.", ch,
-	   NULL, argument , TO_ROOM );
-      add_timer ( ch , TIMER_DO_FUN , 10 , do_makecomlink , SUB_PAUSE );
-      ch->dest_buf = str_dup(arg);
-      ch->dest_buf_2 = str_dup(arg2);
-    }
-  else
-    {
-      send_to_char("&RYou can't figure out how to fit the parts together.\r\n",ch);
-      learn_from_failure( ch, gsn_makecomlink );
-    }
+  return recipe;
 }
 
-static void OnFinished( Character *ch )
+static void InterpretArgumentsHandler( void *userData, InterpretArgumentsEventArgs *args )
 {
-  char arg[MAX_INPUT_LENGTH];
-  char arg2[MAX_INPUT_LENGTH];
+  Character *ch = GetEngineer( args->CraftingSession );
+  char originalArgs[MAX_INPUT_LENGTH];
+  char *argument = originalArgs;
+  char wearloc[MAX_STRING_LENGTH];
+  char itemname[MAX_STRING_LENGTH];
+
+  strcpy( argument, args->CommandArguments );
+  argument = one_argument( argument, wearloc );
+  strcpy( itemname, argument );
+
+  if ( itemname[0] == '\0' )
+    {
+      ch_printf( ch, "&RUsage: Makecomlink <wearloc> <name>\r\n&w" );
+      args->AbortSession = true;
+      return;
+    }
+
+  if ( !str_cmp( wearloc, "body" )
+       || !str_cmp( wearloc, "head" )
+       || !str_cmp( wearloc, "legs" )
+       || !str_cmp( wearloc, "arms" )
+       || !str_cmp( wearloc, "about" )
+       || !str_cmp( wearloc, "eyes" )
+       || !str_cmp( wearloc, "waist" )
+       || !str_cmp( wearloc, "hold" )
+       || !str_cmp( wearloc, "feet" )
+       || !str_cmp( wearloc, "hands" ) )
+    {
+      ch_printf( ch, "&RYou cannot make a comlink for that body part.\r\n&w" );
+      args->AbortSession = true;
+      return;
+    }
+
+  if ( !str_cmp( wearloc, "shield" ) )
+    {
+      ch_printf( ch, "&RYou cannot make a comlink worn as a shield.\r\n&w" );
+      ch_printf( ch, "&RTry MAKESHIELD.\r\n&w" );
+      args->AbortSession = true;
+      return;
+    }
+
+  if ( !str_cmp( wearloc, "wield" ) )
+    {
+      ch_printf( ch, "&RAre you going to fight with your comlink?\r\n&w" );
+      args->AbortSession = true;
+      return;
+    }
+
+  AddCraftingArgument( args->CraftingSession, wearloc );
+  AddCraftingArgument( args->CraftingSession, itemname );
+}
+
+static void SetObjectStatsHandler( void *userData, SetObjectStatsEventArgs *args )
+{
+  const char *wearLoc = GetCraftingArgument( args->CraftingSession, WearLocation );
+  const char *itemName = GetCraftingArgument( args->CraftingSession, ItemName );
   char buf[MAX_STRING_LENGTH];
-  int the_chance = 0;
-  bool checktool = false;
-  bool checkgem = false;
-  bool checkbatt = false;
-  bool checkcirc = false;
-  OBJ_DATA *obj = NULL;
+  OBJ_DATA *obj = args->Object;
   int value = 0;
-  OBJ_INDEX_DATA *pObjIndex = NULL;
-  vnum_t vnum = INVALID_VNUM;
-  long xpgain = 0;
 
-  if ( !ch->dest_buf )
-    return;
-
-  strcpy(arg, (const char*)ch->dest_buf);
-  DISPOSE( ch->dest_buf);
-
-  if ( !ch->dest_buf_2 )
-    return;
-
-  strcpy(arg2, (const char*)ch->dest_buf_2);
-  DISPOSE( ch->dest_buf_2);
-
-  vnum = OBJ_VNUM_CRAFTING_COMLINK;
-
-  if ( ( pObjIndex = get_obj_index( vnum ) ) == NULL )
-    {
-      send_to_char( "&RThe item you are trying to create is missing from the database.\r\nPlease inform the administration of this error.\r\n", ch );
-      return;
-    }
-
-  for ( obj = ch->last_carrying; obj; obj = obj->prev_content )
-    {
-      if (obj->item_type == ITEM_TOOLKIT)
-        checktool = true;
-
-      if (obj->item_type == ITEM_CRYSTAL && checkgem == false)
-        {
-          checkgem = true;
-          separate_obj( obj );
-          obj_from_char( obj );
-          extract_obj( obj );
-        }
-      if (obj->item_type == ITEM_CIRCUIT && checkcirc == false)
-        {
-          checkcirc = true;
-          separate_obj( obj );
-          obj_from_char( obj );
-          extract_obj( obj );
-        }
-      if (obj->item_type == ITEM_BATTERY && checkbatt == false )
-        {
-          separate_obj( obj );
-          obj_from_char( obj );
-          extract_obj( obj );
-          checkbatt = true;
-	}
-    }
-
-  the_chance = is_npc(ch) ? ch->top_level
-    : (int) (ch->pcdata->learned[gsn_makecomlink]) ;
-
-  if ( number_percent( ) > the_chance*2  || ( !checktool ) || ( !checkcirc ) || ( !checkbatt ) || ( !checkgem ) )
-    {
-      send_to_char( "&RYou hold up your newly created comlink....\r\n", ch);
-      send_to_char( "&Rand it falls apart in your hands.\r\n", ch);
-      learn_from_failure( ch, gsn_makecomlink );
-      return;
-    }
-
-  obj = create_object( pObjIndex, ch->top_level );
-
-  obj->item_type = ITEM_COMLINK;
   SET_BIT( obj->wear_flags, ITEM_TAKE );
-  value = get_wearflag( arg );
+  value = get_wearflag( wearLoc );
 
   if ( value < 0 || value > 31 )
-    SET_BIT( obj->wear_flags, ITEM_WEAR_NECK );
+    {
+      SET_BIT( obj->wear_flags, ITEM_HOLD );
+    }
   else
-    SET_BIT( obj->wear_flags, 1 << value );
+    {
+      SET_BIT( obj->wear_flags, 1 << value );
+    }
 
   obj->weight = 1;
+
   STRFREE( obj->name );
-  strcpy( buf, arg2 );
+  strcpy( buf, itemName );
   strcat( buf, " comlink" );
   obj->name = STRALLOC( buf );
-  strcpy( buf, arg2 );
+
+  strcpy( buf, itemName );
   STRFREE( obj->short_descr );
   obj->short_descr = STRALLOC( buf );
+
   STRFREE( obj->description );
   strcat( buf, " was left here." );
-  obj->description = STRALLOC( buf );
+  obj->description = STRALLOC( capitalize( buf ) );
+
   obj->cost = 50;
-
-  obj = obj_to_char( obj, ch );
-
-  send_to_char( "&GYou finish your work and hold up your newly created comlink.&w\r\n", ch);
-  act( AT_PLAIN, "$n finishes crafting a comlink.", ch,
-       NULL, NULL , TO_ROOM );
-
-  xpgain = UMIN( obj->cost*100 ,( exp_level(get_level(ch, ENGINEERING_ABILITY ) + 1 ) - exp_level(get_level(ch, ENGINEERING_ABILITY ) ) ) );
-  gain_exp(ch, ENGINEERING_ABILITY, xpgain );
-  ch_printf( ch , "You gain %d engineering experience.", xpgain );
-
-  learn_from_success( ch, gsn_makecomlink );
-}
-
-static void OnAbort( Character *ch )
-{
-  DISPOSE( ch->dest_buf );
-  send_to_char("&RYou are interupted and fail to finish your work.\r\n", ch);
 }
