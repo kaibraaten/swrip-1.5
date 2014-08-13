8e61c88cadb6033c897ca5247841c22e3df6a317
Kai Braaten <kai.braaten@gmail.com>
2014-08-13 22:58:16 +0000

    sdf

--- src/commands/makebowcaster.c
+++ src/commands/makebowcaster.c
@@ -1,294 +1,169 @@
 #include <string.h>
 #include "mud.h"
 #include "character.h"
+#include "craft.h"
 
-static void OnStart( Character *ch, char *argument );
-static void OnFinished( Character *ch );
-static void OnAbort( Character *ch );
+static void InterpretArgumentsHandler( void *userData, InterpretArgumentsEventArgs *args );
+static void CheckRequirementsHandler( void *userData, CheckRequirementsEventArgs *args );
+static void MaterialFoundHandler( void *userData, MaterialFoundEventArgs *args );
+static void SetObjectStatsHandler( void *userData, SetObjectStatsEventArgs *args );
+static void FinishedCraftingHandler( void *userData, FinishedCraftingEventArgs *args );
+static void AbortHandler( void *userData, AbortCraftingEventArgs *args );
+static CraftRecipe *CreateMakeBowcasterCraftRecipe( void );
+
+struct UserData
+{
+  int Ammo;
+  int Tinder;
+  int Lenses;
+};
 
 void do_makebowcaster( Character *ch, char *argument )
 {
-  switch( ch->substate )
+  struct UserData *data = NULL;
+  CraftRecipe *recipe = CreateMakeBowcasterCraftRecipe();
+  CraftingSession *session = AllocateCraftingSession( recipe, ch, argument );
+
+  CREATE( data, struct UserData, 1 );
+
+  AddInterpretArgumentsCraftingHandler( session, data, InterpretArgumentsHandler );
+  AddCheckRequirementsCraftingHandler( session, data, CheckRequirementsHandler );
+  AddMaterialFoundCraftingHandler( session, data, MaterialFoundHandler );
+  AddSetObjectStatsCraftingHandler( session, data, SetObjectStatsHandler );
+  AddFinishedCraftingHandler( session, data, FinishedCraftingHandler );
+  AddAbortCraftingHandler( session, data, AbortHandler );
+
+  StartCrafting( session );
+}
+
+static CraftRecipe *CreateMakeBowcasterCraftRecipe( void )
+{
+  static const struct CraftingMaterial materials[] =
     {
-    default:
-      OnStart( ch, argument );
-      break;
+      { ITEM_TOOLKIT,    CRAFTFLAG_NONE },
+      { ITEM_OVEN,       CRAFTFLAG_NONE },
+      { ITEM_DURAPLAST,  CRAFTFLAG_EXTRACT },
+      { ITEM_OIL,        CRAFTFLAG_EXTRACT },
+      { ITEM_TINDER,     CRAFTFLAG_EXTRACT },
+      { ITEM_CROSSBOW,   CRAFTFLAG_EXTRACT },
+      { ITEM_LENS,       CRAFTFLAG_EXTRACT | CRAFTFLAG_OPTIONAL },
+      { ITEM_BOLT,       CRAFTFLAG_EXTRACT | CRAFTFLAG_OPTIONAL },
+      { ITEM_NONE,       CRAFTFLAG_NONE }
+    };
+  CraftRecipe *recipe = AllocateCraftRecipe( gsn_makebowcaster, materials,
+					     25, OBJ_VNUM_CRAFTING_BOWCASTER,
+					     CRAFTFLAG_NEED_WORKSHOP );
 
-    case SUB_PAUSE:
-      ch->substate = SUB_NONE;
-      OnFinished( ch );
-      break;
+  return recipe;
+}
 
-    case SUB_TIMER_DO_ABORT:
-      ch->substate = SUB_NONE;
-      OnAbort( ch );
-      break;
+static void InterpretArgumentsHandler( void *userData, InterpretArgumentsEventArgs *args )
+{
+  Character *ch = GetEngineer( args->CraftingSession );
+
+  if ( args->CommandArguments[0] == '\0' )
+    {
+      ch_printf( ch, "&RUsage: Makebowcaster <name>\r\n&w" );
+      args->AbortSession = true;
+      return;
+    }
+
+  AddCraftingArgument( args->CraftingSession, args->CommandArguments );
+}
+
+static void CheckRequirementsHandler( void *userData, CheckRequirementsEventArgs *args )
+{
+  Character *ch = GetEngineer( args->CraftingSession );
+
+  if( ch->race != RACE_WOOKIEE && !is_immortal( ch ) )
+    {
+      ch_printf( ch, "&ROnly wookiees have the knowledge to craft bowcasters.\r\n&w" );
+      args->AbortSession = true;
     }
 }
 
-static void OnStart( Character *ch, char *argument )
+static void MaterialFoundHandler( void *userData, MaterialFoundEventArgs *args )
 {
-  char arg[MAX_INPUT_LENGTH];
-  int the_chance = 0;
-  bool checktool = false;
-  bool checkoven = false;
-  bool checkdura = false;
-  bool checkbow = false;
-  bool checktinder = false;
-  bool checkoil = false;
-  OBJ_DATA *obj = NULL;
+  struct UserData *ud = (struct UserData*) userData;
 
-  strcpy( arg , argument );
-
-  if ( ch->race != RACE_WOOKIEE )
+  if( args->Object->item_type == ITEM_BOLT )
     {
-      send_to_char( "&ROnly wookiees have the knowledge to craft bowcasters.\r\n&w", ch);
-      return;
+      ud->Ammo = args->Object->value[OVAL_BOLT_CHARGE];
     }
 
-  if ( arg[0] == '\0' )
+  if( args->Object->item_type == ITEM_LENS && ud->Lenses < 2 )
     {
-      send_to_char( "&RUsage: Makebowcaster <name>\r\n&w", ch);
-      return;
+      ++ud->Lenses;
+      args->KeepFinding = ud->Lenses < 2;
     }
 
-  if ( !IS_SET( ch->in_room->room_flags, ROOM_FACTORY ) )
+  if( args->Object->item_type == ITEM_TINDER && ud->Tinder < 4 )
     {
-      send_to_char( "&RYou need to be in a factory or workshop to do that.\r\n", ch);
-      return;
-    }
-
-  for ( obj = ch->last_carrying; obj; obj = obj->prev_content )
-    {
-      if (obj->item_type == ITEM_TOOLKIT)
-	checktool = true;
-      if (obj->item_type == ITEM_DURAPLAST)
-	checkdura = true;
-      if (obj->item_type == ITEM_CROSSBOW)
-	checkbow = true;
-      if (obj->item_type == ITEM_OVEN)
-	checkoven = true;
-      if (obj->item_type == ITEM_TINDER)
-	checktinder = true;
-      if (obj->item_type == ITEM_OIL)
-	checkoil = true;
-    }
-
-  if ( !checktool )
-    {
-      send_to_char( "&RYou need toolkit to make a blaster.\r\n", ch);
-      return;
-    }
-
-  if ( !checkdura )
-    {
-      send_to_char( "&RYou need something to make it out of.\r\n", ch);
-      return;
-    }
-
-  if ( !checkoil )
-    {
-      send_to_char( "&RYou need something to greeze the wood with.\r\n", ch);
-      return;
-    }
-
-  if ( !checkoven )
-    {
-      send_to_char( "&RYou need a small furnace to heat the plastics.\r\n", ch);
-      return;
-    }
-
-  if ( !checkbow )
-    {
-      send_to_char( "&RYou need a crossbow to start with.\r\n", ch);
-      return;
-    }
-
-  if ( !checktinder )
-    {
-      send_to_char( "&RYou need wood to complete your bowcaster.\r\n", ch);
-      return;
-    }
-
-  the_chance = is_npc(ch) ? ch->top_level
-    : (int) (ch->pcdata->learned[gsn_makebowcaster]);
-
-  if ( number_percent( ) < the_chance )
-    {
-      send_to_char( "&GYou begin the long process of making a bowcaster.\r\n", ch);
-      act( AT_PLAIN, "$n takes $s tools and a small oven and begins to work on something.", ch,
-	   NULL, argument , TO_ROOM );
-      add_timer ( ch , TIMER_DO_FUN , 25 , do_makebowcaster , SUB_PAUSE );
-      ch->dest_buf   = str_dup(arg);
-      return;
-    }
-  else
-    {
-      send_to_char("&RYou can't figure out how to fit the parts together.\r\n",ch);
-      learn_from_failure( ch, gsn_makebowcaster );
+      ++ud->Tinder;
+      args->KeepFinding = ud->Tinder < 4;
     }
 }
 
-static void OnFinished( Character *ch )
+static void SetObjectStatsHandler( void *userData, SetObjectStatsEventArgs *args )
 {
-  char arg[MAX_INPUT_LENGTH];
+  struct UserData *ud = (struct UserData*) userData;
+  const char *itemName = GetCraftingArgument( args->CraftingSession, 0 );
   char buf[MAX_STRING_LENGTH];
-  long xpgain = 0;
-  int level = 0;
-  int the_chance = 0;
-  bool checktool = false;
-  bool checkoven = false;
-  bool checkdura = false;
-  bool checkbow = false;
-  bool checktinder = false;
-  bool checkoil = false;
-  bool checkammo = false;
-  OBJ_DATA *obj = NULL;
-  OBJ_INDEX_DATA *pObjIndex = NULL;
-  int vnum = INVALID_VNUM;
-  int power = 0;
-  int scope = 0;
-  int ammo = 0;
-  Affect *paf = NULL;
-  Affect *paf2 = NULL;
-
-  if ( !ch->dest_buf )
-    return;
-
-  strcpy(arg, (const char*)ch->dest_buf);
-  DISPOSE( ch->dest_buf);
-
-  level = is_npc(ch) ? ch->top_level : (int) (ch->pcdata->learned[gsn_makebowcaster]);
-  vnum = OBJ_VNUM_CRAFTING_BOWCASTER;
-
-  if ( ( pObjIndex = get_obj_index( vnum ) ) == NULL )
-    {
-      send_to_char( "&RThe item you are trying to create is missing from the database.\r\nPlease inform the administration of this error.\r\n", ch );
-      return;
-    }
-
-  for ( obj = ch->last_carrying; obj; obj = obj->prev_content )
-    {
-      if (obj->item_type == ITEM_TOOLKIT)
-        checktool = true;
-      if (obj->item_type == ITEM_OVEN)
-        checkoven = true;
-      if (obj->item_type == ITEM_DURAPLAST && checkdura == false)
-        {
-          checkdura = true;
-          separate_obj( obj );
-          obj_from_char( obj );
-          extract_obj( obj );
-        }
-      if (obj->item_type == ITEM_BOLT && checkammo == false)
-        {
-          ammo = obj->value[OVAL_BOLT_CHARGE];
-          checkammo = true;
-          separate_obj( obj );
-          obj_from_char( obj );
-          extract_obj( obj );
-        }
-      if (obj->item_type == ITEM_OIL && checkoil == false)
-        {
-	  separate_obj( obj );
-          obj_from_char( obj );
-          extract_obj( obj );
-          checkoil = true;
-        }
-      if (obj->item_type == ITEM_LENS && scope < 2)
-        {
-          separate_obj( obj );
-          obj_from_char( obj );
-          extract_obj( obj );
-          scope++;
-        }
-      if (obj->item_type == ITEM_TINDER && power<4)
-        {
-          power++;
-          separate_obj( obj );
-          obj_from_char( obj );
-          extract_obj( obj );
-          checktinder = true;
-        }
-      if (obj->item_type == ITEM_CROSSBOW && checkbow == false)
-        {
-          separate_obj( obj );
-          obj_from_char( obj );
-          extract_obj( obj );
-          checkbow = true;
-        }
-    }
-
-  the_chance = is_npc(ch) ? ch->top_level
-    : (int) (ch->pcdata->learned[gsn_makebowcaster]) ;
-
-  if ( number_percent( ) > the_chance*2  || ( !checktool ) || ( !checkdura ) || ( !checkoil ) || ( !checkoven )  || ( !checktinder ) || ( !checkbow) )
-    {
-      send_to_char( "&RYou hold up your new bowcaster and aim at a leftover piece of plastic.\r\n", ch);
-      send_to_char( "&RYou slowly draw back the bolt and squeeze the trigger hoping for the best...\r\n", ch);
-      send_to_char( "&RYour bowcaster backfires destroying the firing mechanism and ruining your work.\r\n", ch);
-      learn_from_failure( ch, gsn_makebowcaster );
-      return;
-    }
-
-  obj = create_object( pObjIndex, level );
+  OBJ_DATA *obj = args->Object;
+  Affect *hitroll = NULL;
+  Affect *damroll = NULL;
 
   obj->item_type = ITEM_WEAPON;
   SET_BIT( obj->wear_flags, ITEM_WIELD );
   SET_BIT( obj->wear_flags, ITEM_TAKE );
-  obj->level = level;
-  obj->weight = 2+level/7;
+  obj->weight = 2 + obj->level / 7;
+
   STRFREE( obj->name );
-  strcpy( buf , arg );
-  strcat( buf , " bowcaster");
+  strcpy( buf, itemName );
+  strcat( buf, " bowcaster");
   obj->name = STRALLOC( buf );
-  strcpy( buf, arg );
+
+  strcpy( buf, itemName );
   STRFREE( obj->short_descr );
   obj->short_descr = STRALLOC( buf );
+
   STRFREE( obj->description );
   strcat( buf, " was carefully placed here." );
-  obj->description = STRALLOC( buf );
-  CREATE( paf, Affect, 1 );
-  paf->type               = -1;
-  paf->duration           = -1;
-  paf->location           = get_affecttype( "hitroll" );
-  paf->modifier           = URANGE( 0, 1+scope, level/30 );
-  paf->bitvector          = 0;
-  paf->next               = NULL;
-  LINK( paf, obj->first_affect, obj->last_affect, next, prev );
+  obj->description = STRALLOC( capitalize( buf ) );
+
+  CREATE( hitroll, Affect, 1 );
+  hitroll->type      = -1;
+  hitroll->duration  = -1;
+  hitroll->location  = get_affecttype( "hitroll" );
+  hitroll->modifier  = urange( 0, 1 + ud->Lenses, obj->level / 30 );
+  LINK( hitroll, obj->first_affect, obj->last_affect, next, prev );
   ++top_affect;
-  CREATE( paf2, Affect, 1 );
-  paf2->type               = -1;
-  paf2->duration           = -1;
-  paf2->location           = get_affecttype( "damroll" );
-  paf2->modifier           = URANGE( 0, power, level/30);
-  paf2->bitvector          = 0;
-  paf2->next               = NULL;
-  LINK( paf2, obj->first_affect, obj->last_affect, next, prev );
+
+  CREATE( damroll, Affect, 1 );
+  damroll->type      = -1;
+  damroll->duration  = -1;
+  damroll->location  = get_affecttype( "damroll" );
+  damroll->modifier  = urange( 0, ud->Tinder, obj->level / 30);
+  LINK( damroll, obj->first_affect, obj->last_affect, next, prev );
   ++top_affect;
+
   obj->value[OVAL_WEAPON_CONDITION] = INIT_WEAPON_CONDITION;
-  obj->value[OVAL_WEAPON_NUM_DAM_DIE] = (int) (level/10+25);
-  obj->value[OVAL_WEAPON_SIZE_DAM_DIE] = (int) (level/5+25);
+  obj->value[OVAL_WEAPON_NUM_DAM_DIE] = (int) (obj->level / 10 + 25);
+  obj->value[OVAL_WEAPON_SIZE_DAM_DIE] = (int) (obj->level / 5 + 25);
   obj->value[OVAL_WEAPON_TYPE] = WEAPON_BOWCASTER;
-  obj->value[OVAL_WEAPON_CHARGE] = ammo;
+  obj->value[OVAL_WEAPON_CHARGE] = ud->Ammo;
   obj->value[OVAL_WEAPON_MAX_CHARGE] = 250;
-  obj->cost = obj->value[OVAL_WEAPON_SIZE_DAM_DIE]*50;
-
-  obj = obj_to_char( obj, ch );
-
-  send_to_char( "&GYou finish your work and hold up your newest bowcaster.&w\r\n", ch);
-  act( AT_PLAIN, "$n finishes making $s new bowcaster.", ch,
-       NULL, NULL , TO_ROOM );
-
-  xpgain = UMIN( obj->cost*50 ,( exp_level(get_level(ch, ENGINEERING_ABILITY) + 1) - exp_level(get_level(ch, ENGINEERING_ABILITY ) ) ) );
-  gain_exp(ch, ENGINEERING_ABILITY, xpgain );
-  ch_printf( ch , "You gain %d engineering experience.", xpgain );
-  
-  learn_from_success( ch, gsn_makebowcaster );
+  obj->cost = obj->value[OVAL_WEAPON_SIZE_DAM_DIE] * 50;
 }
 
-static void OnAbort( Character *ch )
+static void FinishedCraftingHandler( void *userData, FinishedCraftingEventArgs *args )
 {
-  DISPOSE( ch->dest_buf );
-  send_to_char("&RYou are interupted and fail to finish your work.\r\n", ch);
+  struct UserData *ud = (struct UserData*) userData;
+  DISPOSE( ud );
+}
+
+static void AbortHandler( void *userData, AbortCraftingEventArgs *args )
+{
+  struct UserData *ud = (struct UserData*) userData;
+  DISPOSE( ud );
 }
