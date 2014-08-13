f6258ad55dda81d9d2fbe5b2f604aaa5ffe2a7c4
Kai Braaten <kai.braaten@gmail.com>
2014-08-13 13:55:33 +0000

    sdf

--- src/commands/makecontainer.c
+++ src/commands/makecontainer.c
@@ -1,46 +1,111 @@
 #include <string.h>
-#include "mud.h"
-#include "character.h"
-#include "craft.h"
+#include <stdlib.h>
 
-static void OnStart( Character *ch, char *argument );
-static void OnFinished( Character *ch );
-static void OnAbort( Character *ch );
+#include "mud.h"
+#include "craft.h"
+#include "character.h"
+
+enum { WearLocation, ItemName };
+
+struct UserData
+{
+  int Dummy;
+};
+
+static void InterpretArgumentsHandler( void *userData, InterpretArgumentsEventArgs *args );
+static void CheckRequirementsHandler( void *userData, CheckRequirementsEventArgs *args );
+static void MaterialFoundHandler( void *userData, MaterialFoundEventArgs *args );
+static void SetObjectStatsHandler( void *userData, SetObjectStatsEventArgs *args );
+static void FinishedCraftingHandler( void *userData, FinishedCraftingEventArgs *args );
+static void AbortHandler( void *userData, AbortCraftingEventArgs *args );
 
 void do_makecontainer( Character *ch, char *argument )
 {
-  switch( ch->substate )
+  struct UserData *data;
+  static const struct CraftingMaterial materials[] =
     {
-    default:
-      OnStart( ch, argument );
-      break;
+      { ITEM_FABRIC, CRAFTFLAG_EXTRACT },
+      { ITEM_THREAD, CRAFTFLAG_NONE },
+      { ITEM_NONE,   CRAFTFLAG_NONE }
+    };
 
-    case SUB_PAUSE:
-      ch->substate = SUB_NONE;
-      OnFinished( ch );
-      break;
+  CraftRecipe *recipe = AllocateCraftRecipe( gsn_makecontainer, materials,
+                                             10, OBJ_VNUM_CRAFTING_CONTAINER );
+  CraftingSession *session = AllocateCraftingSession( recipe, ch, argument );
 
-    case SUB_TIMER_DO_ABORT:
-      ch->substate = SUB_NONE;
-      OnAbort( ch );
-      break;
-    }
+  CREATE( data, struct UserData, 1 );
+
+  AddInterpretArgumentsCraftingHandler( session, data, InterpretArgumentsHandler );
+  AddCheckRequirementsCraftingHandler( session, data, CheckRequirementsHandler );
+  AddMaterialFoundCraftingHandler( session, data, MaterialFoundHandler );
+  AddSetObjectStatsCraftingHandler( session, data, SetObjectStatsHandler );
+  AddFinishedCraftingHandler( session, data, FinishedCraftingHandler );
+  AddAbortCraftingHandler( session, data, AbortHandler );
+
+  StartCrafting( session );
 }
 
-static void OnStart( Character *ch, char *argument )
+static void MaterialFoundHandler( void *userData, MaterialFoundEventArgs *args )
 {
-  char arg[MAX_INPUT_LENGTH];
-  char arg2[MAX_INPUT_LENGTH];
-  int the_chance = 0;
-  bool checksew = false, checkfab = false;
-  OBJ_DATA *obj = NULL;
 
+}
+
+static void FinishedCraftingHandler( void *userData, FinishedCraftingEventArgs *args )
+{
+  struct UserData *data = (struct UserData*) userData;
+  DISPOSE( data );
+}
+
+static void SetObjectStatsHandler( void *userData, SetObjectStatsEventArgs *eventArgs )
+{
+  OBJ_DATA *obj = eventArgs->Object;
+  const char *wearLocation = GetCraftingArgument( eventArgs->CraftingSession, WearLocation );
+  const char *itemName = GetCraftingArgument( eventArgs->CraftingSession, ItemName );
+  char description[MAX_STRING_LENGTH];
+  int value = 0;
+
+  obj->item_type = ITEM_CONTAINER;
+  SET_BIT( obj->wear_flags, ITEM_TAKE );
+  value = get_wearflag( wearLocation );
+
+  if ( value < 0 || value > 31 )
+    SET_BIT( obj->wear_flags, ITEM_HOLD );
+  else
+    SET_BIT( obj->wear_flags, 1 << value );
+
+  STRFREE( obj->name );
+  obj->name = STRALLOC( itemName );
+
+  STRFREE( obj->short_descr );
+  obj->short_descr = STRALLOC( itemName );
+
+  STRFREE( obj->description );
+  sprintf( description, "%s was dropped here.", capitalize( itemName ) );
+  obj->description = STRALLOC( description );
+
+  obj->value[0] = obj->level;
+  obj->value[1] = 0;
+  obj->value[2] = 0;
+  obj->value[3] = 10;
+}
+
+static void InterpretArgumentsHandler( void *userData, InterpretArgumentsEventArgs *eventArgs )
+{
+  CraftingSession *session = eventArgs->CraftingSession;
+  char originalArgs[MAX_INPUT_LENGTH];
+  char *argument = originalArgs;
+  char arg[MAX_STRING_LENGTH];
+  char arg2[MAX_STRING_LENGTH];
+  Character *ch = GetEngineer( session );
+
+  strcpy( argument, eventArgs->CommandArguments );
   argument = one_argument( argument, arg );
-  strcpy( arg2 , argument );
+  strcpy( arg2, argument );
 
   if ( arg2[0] == '\0' )
     {
       send_to_char( "&RUsage: Makecontainer <wearloc> <name>\r\n&w", ch);
+      eventArgs->AbortSession = true;
       return;
     }
 
@@ -54,6 +119,7 @@ static void OnStart( Character *ch, char *argument )
     {
       send_to_char( "&RYou cannot make a container for that body part.\r\n&w", ch);
       send_to_char( "&RTry MAKEJEWELRY.\r\n&w", ch);
+      eventArgs->AbortSession = true;
       return;
     }
 
@@ -62,6 +128,7 @@ static void OnStart( Character *ch, char *argument )
     {
       send_to_char( "&RYou cannot make a container for that body part.\r\n&w", ch);
       send_to_char( "&RTry MAKEARMOR.\r\n&w", ch);
+      eventArgs->AbortSession = true;
       return;
     }
 
@@ -69,6 +136,7 @@ static void OnStart( Character *ch, char *argument )
     {
       send_to_char( "&RYou cannot make a container a shield.\r\n&w", ch);
       send_to_char( "&RTry MAKESHIELD.\r\n&w", ch);
+      eventArgs->AbortSession = true;
       return;
     }
 
@@ -76,149 +144,28 @@ static void OnStart( Character *ch, char *argument )
     {
       send_to_char( "&RAre you going to fight with a container?\r\n&w", ch);
       send_to_char( "&RTry MAKEBLADE...\r\n&w", ch);
+      eventArgs->AbortSession = true;
       return;
     }
+
+  AddCraftingArgument( session, arg );
+  AddCraftingArgument( session, arg2 );
+}
+
+static void AbortHandler( void *userData, AbortCraftingEventArgs *args )
+{
+  struct UserData *data = (struct UserData*) userData;
+  DISPOSE( data );
+}
+
+static void CheckRequirementsHandler( void *userData, CheckRequirementsEventArgs *eventArgs )
+{
+  Character *ch = GetEngineer( eventArgs->CraftingSession );
 
   if ( !IS_SET( ch->in_room->room_flags, ROOM_FACTORY ) )
     {
-      send_to_char( "&RYou need to be in a factory or workshop to do that.\r\n", ch);
+      ch_printf( ch, "&RYou need to be in a factory or workshop to do that.&w\r\n" );
+      eventArgs->AbortSession = true;
       return;
     }
-
-  for ( obj = ch->last_carrying; obj; obj = obj->prev_content )
-    {
-      if (obj->item_type == ITEM_FABRIC)
-	checkfab = true;
-      if (obj->item_type == ITEM_THREAD)
-	checksew = true;
-    }
-
-  if ( !checkfab )
-    {
-      send_to_char( "&RYou need some sort of fabric or material.\r\n", ch);
-      return;
-    }
-
-  if ( !checksew )
-    {
-      send_to_char( "&RYou need a needle and some thread.\r\n", ch);
-      return;
-    }
-
-  the_chance = is_npc(ch) ? ch->top_level
-    : (int) (ch->pcdata->learned[gsn_makecontainer]);
-
-  if ( number_percent( ) < the_chance )
-    {
-      send_to_char( "&GYou begin the long process of creating a bag.\r\n", ch);
-      act( AT_PLAIN, "$n takes $s sewing kit and some material and begins to work.", ch,
-	   NULL, argument , TO_ROOM );
-      add_timer ( ch , TIMER_DO_FUN , 10 , do_makecontainer , SUB_PAUSE );
-      ch->dest_buf = str_dup(arg);
-      ch->dest_buf_2 = str_dup(arg2);
-      return;
-    }
-  else
-    {
-      send_to_char("&RYou can't figure out what to do.\r\n",ch);
-      learn_from_failure( ch, gsn_makecontainer );
-    }
-}
-
-static void OnFinished( Character *ch )
-{
-  char arg[MAX_INPUT_LENGTH];
-  char arg2[MAX_INPUT_LENGTH];
-  char buf[MAX_STRING_LENGTH];
-  int level = 0, the_chance = 0;
-  bool checksew = false, checkfab = false;
-  OBJ_DATA *obj = NULL;
-  OBJ_DATA *material = NULL;
-  int value = 0;
-  long xpgain = 0;
-
-  if ( !ch->dest_buf )
-    return;
-
-  if ( !ch->dest_buf_2 )
-    return;
-
-  strcpy(arg, (const char*)ch->dest_buf);
-  DISPOSE( ch->dest_buf);
-  strcpy(arg2, (const char*)ch->dest_buf_2);
-  DISPOSE( ch->dest_buf_2);
-
-  level = is_npc(ch) ? ch->top_level : (int) (ch->pcdata->learned[gsn_makecontainer]);
-
-  for ( obj = ch->last_carrying; obj; obj = obj->prev_content )
-    {
-      if (obj->item_type == ITEM_THREAD)
-        checksew = true;
-
-      if (obj->item_type == ITEM_FABRIC && checkfab == false)
-        {
-          checkfab = true;
-          separate_obj( obj );
-          obj_from_char( obj );
-          material = obj;
-        }
-    }
-
-  the_chance = is_npc(ch) ? ch->top_level
-    : (int) (ch->pcdata->learned[gsn_makecontainer]) ;
-
-  if ( number_percent( ) > the_chance*2  || ( !checkfab ) || ( !checksew ) )
-    {
-      send_to_char( "&RYou hold up your newly created container.\r\n", ch);
-      send_to_char( "&RIt suddenly dawns upon you that you have created the most useless\r\n", ch);
-      send_to_char( "&Rcontainer you've ever seen. You quickly hide your mistake...\r\n", ch);
-      learn_from_failure( ch, gsn_makecontainer );
-      return;
-    }
-
-  obj = material;
-
-  obj->item_type = ITEM_CONTAINER;
-  SET_BIT( obj->wear_flags, ITEM_TAKE );
-  value = get_wearflag( arg );
-
-  if ( value < 0 || value > 31 )
-    SET_BIT( obj->wear_flags, ITEM_HOLD );
-  else
-    SET_BIT( obj->wear_flags, 1 << value );
-
-  obj->level = level;
-  STRFREE( obj->name );
-  strcpy( buf, arg2 );
-  obj->name = STRALLOC( buf );
-  strcpy( buf, arg2 );
-  STRFREE( obj->short_descr );
-  obj->short_descr = STRALLOC( buf );
-  STRFREE( obj->description );
-  strcat( buf, " was dropped here." );
-  obj->description = STRALLOC( buf );
-  obj->value[OVAL_CONTAINER_CAPACITY] = level;
-  obj->value[OVAL_CONTAINER_FLAGS] = 0;
-  obj->value[OVAL_CONTAINER_KEY] = INVALID_VNUM;
-  obj->value[OVAL_CONTAINER_CONDITION] = 10;
-  obj->cost *= 2;
-
-  obj = obj_to_char( obj, ch );
-
-  send_to_char( "&GYou finish your work and hold up your newly created container.&w\r\n", ch);
-  act( AT_PLAIN, "$n finishes sewing a new container.", ch,
-       NULL, NULL , TO_ROOM );
-
-  xpgain = UMIN( obj->cost*100 ,( exp_level(get_level(ch, ENGINEERING_ABILITY ) + 1) - exp_level(get_level(ch, ENGINEERING_ABILITY) ) ) );
-  gain_exp(ch, ENGINEERING_ABILITY, xpgain );
-  ch_printf( ch , "You gain %d engineering experience.", xpgain );
-
-  learn_from_success( ch, gsn_makecontainer );
-}
-
-static void OnAbort( Character *ch )
-{
-  DISPOSE( ch->dest_buf );
-  DISPOSE( ch->dest_buf_2 );
-  send_to_char("&RYou are interupted and fail to finish your work.\r\n", ch);
 }
