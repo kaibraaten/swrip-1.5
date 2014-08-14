4cf035109871d53fda68513e5d3415b2454b4ce8
Kai Braaten <kai.braaten@gmail.com>
2014-08-14 02:10:11 +0000

    sdf

--- src/commands/makedisguise.c
+++ src/commands/makedisguise.c
@@ -1,220 +1,162 @@
 #include <string.h>
 #include "mud.h"
 #include "character.h"
+#include "craft.h"
 
-static void OnStart( Character *ch, char *argument );
-static void OnFinished( Character *ch );
-static void OnAbort( Character *ch );
+static void InterpretArgumentsHandler( void *userData, InterpretArgumentsEventArgs *args );
+static void SetObjectStatsHandler( void *userData, SetObjectStatsEventArgs *args );
+static void FinishedCraftingHandler( void *userData, FinishedCraftingEventArgs *args );
+static void AbortHandler( void *userData, AbortCraftingEventArgs *args );
+static CraftRecipe *MakeCraftRecipe( void );
+
+struct UserData
+{
+  int Race;
+  int Sex;
+  char *Name;
+};
 
 void do_makedisguise( Character *ch, char *argument )
 {
-  switch( ch->substate )
-    {
-    default:
-      OnStart( ch, argument );
-      break;
+  struct UserData *data = NULL;
+  CraftRecipe *recipe = MakeCraftRecipe();
+  CraftingSession *session = AllocateCraftingSession( recipe, ch, argument );
 
-    case SUB_PAUSE:
-      ch->substate = SUB_NONE;
-      OnFinished( ch );
-      break;
+  CREATE( data, struct UserData, 1 );
 
-    case SUB_TIMER_DO_ABORT:
-      ch->substate = SUB_NONE;
-      OnAbort( ch );
-      break;
-    }
+  AddInterpretArgumentsCraftingHandler( session, data, InterpretArgumentsHandler );
+  AddSetObjectStatsCraftingHandler( session, data, SetObjectStatsHandler );
+  AddFinishedCraftingHandler( session, data, FinishedCraftingHandler );
+  AddAbortCraftingHandler( session, data, AbortHandler );
+
+  StartCrafting( session );
 }
 
-static void OnStart( Character *ch, char *argument )
+static CraftRecipe *MakeCraftRecipe( void )
 {
-  char arg[MAX_INPUT_LENGTH];
-  char arg1[MAX_INPUT_LENGTH];
-  char arg2[MAX_INPUT_LENGTH];
-  char sexrace[MAX_INPUT_LENGTH];
-  int the_chance = 0;
-  bool checkneedle = false, checkfabric = false, checkhair = false;
-  OBJ_DATA *obj = NULL;
-  int sex = 0, race = 0;
-
-  strcpy( arg, argument );
-
-  argument = one_argument( argument, arg1 );
-  argument = one_argument( argument, arg2 );
-
-  if ( !argument || argument[0] == '\0' || arg2[0] == '\0' || arg1[0] == '\0' )
+  static const struct CraftingMaterial materials[] =
     {
-      send_to_char( "&RUsage: Makedisguise <sex> <race> <name>\r\n&w", ch);
-      return;
-    }
+      { ITEM_THREAD,      CRAFTFLAG_NONE },
+      { ITEM_DIS_FABRIC,  CRAFTFLAG_EXTRACT },
+      { ITEM_HAIR,        CRAFTFLAG_EXTRACT },
+      { ITEM_NONE,        CRAFTFLAG_NONE }
+    };
+  CraftRecipe *recipe = AllocateCraftRecipe( gsn_disguise, materials,
+					     25, OBJ_VNUM_CRAFTING_DISGUISE,
+					     CRAFTFLAG_NEED_WORKSHOP );
 
-  if ( !IS_SET( ch->in_room->room_flags, ROOM_FACTORY ) )
-    {
-      send_to_char( "&RYou need to be in a factory or workshop to do that.\r\n", ch);
-      return;
-    }
-
-  for ( obj = ch->last_carrying; obj; obj = obj->prev_content )
-    {
-      if (obj->item_type == ITEM_THREAD)
-	checkneedle = true;
-      if (obj->item_type == ITEM_DIS_FABRIC)
-	checkfabric = true;
-      if (obj->item_type == ITEM_HAIR)
-	checkhair = true;
-    }
-
-  if( (sex = atoi(arg1)) > 2 || sex < 0 )
-    {
-      send_to_char( "&RSex choices: 0 neutral, 1 male, 2 female.\r\n", ch);
-      return;
-    }
-  if( (race = atoi(arg2)) > 33 || sex < 0 )
-    {
-      send_to_char( "&RIncorrect race: Help race for options, from 0 to 33.\r\n", ch);
-      return;
-    }
-
-  if ( !checkneedle )
-    {
-      send_to_char( "&RYou need need and thread to make a disguise.\r\n", ch);
-      return;
-    }
-
-  if ( !checkfabric )
-    {
-      send_to_char( "&RYou need something to make it out of.\r\n", ch);
-      return;
-    }
-
-  if( !checkhair )
-    {
-      ch_printf( ch, "You need some hair to make a disguise.\r\n" );
-      return;
-    }
-
-  sprintf( sexrace, "%d", (sex*1000) + race );
-  the_chance = is_npc(ch) ? ch->top_level
-    : (int) (ch->pcdata->learned[gsn_disguise]);
-
-  if ( number_percent( ) < the_chance )
-    {
-      send_to_char( "&GYou begin the long process of making a disguise.\r\n", ch);
-      act( AT_PLAIN, "$n takes $s tools and a small oven and begins to work on something.", ch,
-	   NULL, NULL, TO_ROOM );
-      add_timer ( ch , TIMER_DO_FUN , 25 , do_makedisguise , SUB_PAUSE );
-      ch->dest_buf   = str_dup(sexrace);
-      ch->dest_buf_2   = str_dup(argument);
-      return;
-    }
-  else
-    {
-      send_to_char("&RYou can't figure out how to fit the parts together.\r\n",ch);
-      learn_from_failure( ch, gsn_disguise );
-    }
+  return recipe;
 }
 
-static void OnFinished( Character *ch )
+static int GetSexFromName( const char *sexName )
 {
-  long xpgain = 0;
-  char arg1[MAX_INPUT_LENGTH];
-  char arg2[MAX_INPUT_LENGTH];
+  int sex = -1;
+
+  if( !str_cmp( sexName, "male" ) )
+    {
+      sex = SEX_MALE;
+    }
+  else if( !str_cmp( sexName, "female" ) )
+    {
+      sex = SEX_FEMALE;
+    }
+  else if( !str_cmp( sexName, "neutral" ) )
+    {
+      sex = SEX_NEUTRAL;
+    }
+
+  return sex;
+}
+
+static void InterpretArgumentsHandler( void *userData, InterpretArgumentsEventArgs *args )
+{
+  struct UserData *ud = (struct UserData*) userData;
+  Character *ch = GetEngineer( args->CraftingSession );
+  char argumentBuf[MAX_INPUT_LENGTH];
+  char *argument = argumentBuf;
+  char sex[MAX_INPUT_LENGTH];
+  char race[MAX_INPUT_LENGTH];
+
+  strcpy( argumentBuf, args->CommandArguments );
+  argument = one_argument( argument, sex );
+  argument = one_argument( argument, race );
+
+  if ( argument[0] == '\0' || sex[0] == '\0' || race[0] == '\0' )
+    {
+      ch_printf( ch, "&RUsage: Makedisguise <sex> <race> <name>\r\n&w" );
+      args->AbortSession = true;
+      return;
+    }
+
+  ud->Sex = GetSexFromName( sex );
+
+  if( ud->Sex < 0 )
+    {
+      ch_printf( ch, "Sex must be male, female or neutral.&w\r\n" );
+      args->AbortSession = true;
+      return;
+    }
+
+  ud->Race = get_race_from_name( race );
+
+  if( ud->Race < 0 )
+    {
+      ch_printf( ch, "&R'%s' is not a valid race.&w\r\n", race );
+      args->AbortSession = true;
+      return;
+    }
+
+  ud->Name = str_dup( argument );
+}
+
+static void SetObjectStatsHandler( void *userData, SetObjectStatsEventArgs *args )
+{
+  struct UserData *ud = (struct UserData*) userData;
   char buf[MAX_STRING_LENGTH];
-  int level = 0, the_chance = 0;
-  bool checkneedle = false, checkfabric = false, checkhair = false;
-  OBJ_DATA *obj = NULL;
-  OBJ_INDEX_DATA *pObjIndex = NULL;
-  vnum_t vnum = INVALID_VNUM;
-  int sex = 0, race = 0;
+  OBJ_DATA *disguise = args->Object;
 
-  if ( !ch->dest_buf )
-    return;
+  SET_BIT( disguise->wear_flags, ITEM_DISGUISE );
+  SET_BIT( disguise->wear_flags, ITEM_TAKE );
 
-  strcpy(arg1, (const char*)ch->dest_buf);
-  DISPOSE( ch->dest_buf);
-  strcpy(arg2, (const char*)ch->dest_buf_2);
-  DISPOSE( ch->dest_buf_2);
+  STRFREE( disguise->name );
+  strcpy( buf, ud->Name );
+  strcat( buf, " disguise");
+  disguise->name = STRALLOC( buf );
 
-  race = atoi(arg1)%1000;
-  sex = atoi(arg1)/1000;
+  strcpy( buf, ud->Name );
+  STRFREE( disguise->short_descr );
+  disguise->short_descr = STRALLOC( buf );
 
-  level = is_npc(ch) ? ch->top_level : (int) (ch->pcdata->learned[gsn_disguise]);
-  vnum = OBJ_VNUM_CRAFTING_DISGUISE;
+  STRFREE( disguise->description );
+  disguise->description = STRALLOC( "" );
 
-  if ( ( pObjIndex = get_obj_index( vnum ) ) == NULL )
-    {
-      send_to_char( "&RThe item you are trying to create is missing from the database.\r\nPlease inform the administration of this error.\r\n", ch );
-      return;
-    }
-
-  for ( obj = ch->last_carrying; obj; obj = obj->prev_content )
-    {
-      if (obj->item_type == ITEM_THREAD)
-        checkneedle = true;
-
-      if (obj->item_type == ITEM_DIS_FABRIC && checkfabric == false)
-        {
-          checkfabric = true;
-          separate_obj( obj );
-          obj_from_char( obj );
-          extract_obj( obj );
-        }
-      if (obj->item_type == ITEM_HAIR && checkhair == false)
-        {
-          checkhair = true;
-          separate_obj( obj );
-          obj_from_char( obj );
-          extract_obj( obj );
-        }
-    }
-
-  the_chance = is_npc(ch) ? ch->top_level
-    : (int) (ch->pcdata->learned[gsn_disguise]) ;
-
-  if ( number_percent() > the_chance*2  || ( !checkneedle ) || ( !checkfabric ) )
-    {
-      send_to_char( "&RYour disguise turns out to be really useless.\r\nYou quickly discard it.\r\n", ch);
-      learn_from_failure( ch, gsn_disguise );
-      return;
-    }
-
-  obj = create_object( pObjIndex, level );
-
-  obj->item_type = ITEM_DISGUISE;
-  SET_BIT( obj->wear_flags, ITEM_DISGUISE );
-  SET_BIT( obj->wear_flags, ITEM_TAKE );
-  obj->level = level;
-  STRFREE( obj->name );
-  strcpy( buf , arg2 );
-  strcat( buf , " disguise");
-  obj->name = STRALLOC( buf );
-  strcpy( buf, arg2 );
-  STRFREE( obj->short_descr );
-  obj->short_descr = STRALLOC( buf );
-  STRFREE( obj->description );
-  obj->description = STRALLOC( "" );
-
-  obj->value[OVAL_DISGUISE_MAX_CONDITION] = INIT_WEAPON_CONDITION;
-  obj->value[OVAL_DISGUISE_CONDITION] = INIT_WEAPON_CONDITION;
-  obj->value[OVAL_DISGUISE_RACE] = race;
-  obj->value[OVAL_DISGUISE_SEX] = sex;
-  obj->cost = 5000;
-
-  obj = obj_to_char( obj, ch );
-
-  send_to_char( "&GYou finish your work and hold up your newly created disguise.&w\r\n", ch);
-  act( AT_PLAIN, "$n finishes making $s new disguise.", ch,
-       NULL, NULL , TO_ROOM );
-
-  xpgain = UMIN( obj->cost*50 ,( exp_level(get_level( ch, ENGINEERING_ABILITY ) + 1) - exp_level(get_level( ch, ENGINEERING_ABILITY ) ) ) );
-  gain_exp(ch, ENGINEERING_ABILITY, xpgain );
-  ch_printf( ch , "You gain %d engineering experience.", xpgain );
-
-  learn_from_success( ch, gsn_disguise );
+  disguise->value[OVAL_DISGUISE_MAX_CONDITION] = INIT_WEAPON_CONDITION;
+  disguise->value[OVAL_DISGUISE_CONDITION] = INIT_WEAPON_CONDITION;
+  disguise->value[OVAL_DISGUISE_RACE] = ud->Race;
+  disguise->value[OVAL_DISGUISE_SEX] = ud->Sex;
+  disguise->cost = 5000;
 }
 
-static void OnAbort( Character *ch )
+static void FinishedCraftingHandler( void *userData, FinishedCraftingEventArgs *args )
 {
-  DISPOSE( ch->dest_buf );
-  send_to_char("&RYou are interupted and fail to finish your work.\r\n", ch);
+  struct UserData *ud = (struct UserData*) userData;
+
+  if( ud->Name )
+    {
+      DISPOSE( ud->Name );
+    }
+
+  DISPOSE( ud );
+}
+
+static void AbortHandler( void *userData, AbortCraftingEventArgs *args )
+{
+  struct UserData *ud = (struct UserData*) userData;
+
+  if( ud->Name )
+    {
+      DISPOSE( ud->Name );
+    }
+
+  DISPOSE( ud );
 }
