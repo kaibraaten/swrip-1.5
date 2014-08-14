deccb5eb6ed9f52822606405fd2233f9addeb2fd
Kai Braaten <kai.braaten@gmail.com>
2014-08-14 14:57:33 +0000

    sdf

--- src/commands/makeglowrod.c
+++ src/commands/makeglowrod.c
@@ -1,229 +1,134 @@
 #include <string.h>
 #include "mud.h"
 #include "character.h"
+#include "craft.h"
 
-static void OnStart( Character *ch, char *argument );
-static void OnFinished( Character *ch );
-static void OnAbort( Character *ch );
+struct UserData
+{
+  char *ItemName;
+  int Charge;
+};
+
+static void InterpretArgumentsHandler( void *userData, InterpretArgumentsEventArgs *args );
+static void CheckRequirementsHandler( void *userData, CheckRequirementsEventArgs *args );
+static void MaterialFoundHandler( void *userData, MaterialFoundEventArgs *args );
+static void SetObjectStatsHandler( void *userData, SetObjectStatsEventArgs *args );
+static void FinishedCraftingHandler( void *userData, FinishedCraftingEventArgs *args );
+static void AbortHandler( void *userData, AbortCraftingEventArgs *args );
+static CraftRecipe *MakeCraftRecipe( void );
+static void FreeUserData( struct UserData *ud );
 
 void do_makeglowrod( Character *ch, char *argument )
 {
-  switch( ch->substate )
+  struct UserData *data = NULL;
+  CraftRecipe *recipe = MakeCraftRecipe();
+  CraftingSession *session = AllocateCraftingSession( recipe, ch, argument );
+
+  CREATE( data, struct UserData, 1 );
+
+  AddInterpretArgumentsCraftingHandler( session, data, InterpretArgumentsHandler );
+  AddCheckRequirementsCraftingHandler( session, data, CheckRequirementsHandler );
+  AddMaterialFoundCraftingHandler( session, data, MaterialFoundHandler );
+  AddSetObjectStatsCraftingHandler( session, data, SetObjectStatsHandler );
+  AddFinishedCraftingHandler( session, data, FinishedCraftingHandler );
+  AddAbortCraftingHandler( session, data, AbortHandler );
+
+  StartCrafting( session );
+}
+
+static CraftRecipe *MakeCraftRecipe( void )
+{
+  static const struct CraftingMaterial materials[] =
     {
-    default:
-      OnStart( ch, argument );
-      break;
+      { ITEM_TOOLKIT,   CRAFTFLAG_NONE },
+      { ITEM_BATTERY,   CRAFTFLAG_EXTRACT },
+      { ITEM_CIRCUIT,   CRAFTFLAG_EXTRACT },
+      { ITEM_CHEMICAL,  CRAFTFLAG_EXTRACT },
+      { ITEM_LENS,      CRAFTFLAG_EXTRACT },
+      { ITEM_NONE,      CRAFTFLAG_NONE }
+    };
+  CraftRecipe *recipe = AllocateCraftRecipe( gsn_makeglowrod, materials,
+					     10, OBJ_VNUM_CRAFTING_GLOWROD,
+					     CRAFTFLAG_NEED_WORKSHOP );
 
-    case SUB_PAUSE:
-      ch->substate = SUB_NONE;
-      OnFinished( ch );
-      break;
+  return recipe;
+}
 
-    case SUB_TIMER_DO_ABORT:
-      ch->substate = SUB_NONE;
-      OnAbort( ch );
-      break;
+static void InterpretArgumentsHandler( void *userData, InterpretArgumentsEventArgs *args )
+{
+  struct UserData *ud = (struct UserData*) userData;
+  Character *ch = GetEngineer( args->CraftingSession );
+
+  if ( args->CommandArguments[0] == '\0' )
+    {
+      ch_printf( ch, "&RUsage: Makeglowrod <name>\r\n&w" );
+      args->AbortSession = true;
+      return;
+    }
+
+  ud->ItemName = str_dup( args->CommandArguments );
+}
+
+static void CheckRequirementsHandler( void *userData, CheckRequirementsEventArgs *args )
+{
+
+}
+
+static void MaterialFoundHandler( void *userData, MaterialFoundEventArgs *args )
+{
+  struct UserData *ud = (struct UserData*) userData;
+
+  if( args->Object->item_type == ITEM_BATTERY )
+    {
+      ud->Charge = args->Object->value[OVAL_BATTERY_CHARGE];
     }
 }
 
-static void OnStart( Character *ch, char *argument )
+static void SetObjectStatsHandler( void *userData, SetObjectStatsEventArgs *args )
 {
-  char arg[MAX_INPUT_LENGTH];
-  int the_chance = 0;
-  bool checktool = false;
-  bool checkbatt = false;
-  bool checkchem = false;
-  bool checkcirc = false;
-  bool checklens = false;
-  OBJ_DATA *obj = NULL;
-
-  strcpy( arg , argument );
-
-  if ( arg[0] == '\0' )
-    {
-      send_to_char( "&RUsage: Makeflashlight <name>\r\n&w", ch);
-      return;
-    }
-
-  if ( !IS_SET( ch->in_room->room_flags, ROOM_FACTORY ) )
-    {
-      send_to_char( "&RYou need to be in a factory or workshop to do that.\r\n", ch);
-      return;
-    }
-
-  for ( obj = ch->last_carrying; obj; obj = obj->prev_content )
-    {
-      if (obj->item_type == ITEM_TOOLKIT)
-	checktool = true;
-      if (obj->item_type == ITEM_BATTERY)
-	checkbatt = true;
-      if (obj->item_type == ITEM_CIRCUIT)
-	checkcirc = true;
-      if (obj->item_type == ITEM_CHEMICAL)
-	checkchem = true;
-      if (obj->item_type == ITEM_LENS)
-	checklens = true;
-    }
-
-  if ( !checktool )
-    {
-      send_to_char( "&RYou need toolkit to make a light.\r\n", ch);
-      return;
-    }
-
-  if ( !checklens )
-    {
-      send_to_char( "&RYou need a lens to make a light.\r\n", ch);
-      return;
-    }
-
-  if ( !checkbatt )
-    {
-      send_to_char( "&RYou need a battery for the light to work.\r\n", ch);
-      return;
-    }
-
-  if ( !checkcirc )
-    {
-      send_to_char( "&RYou need a small circuit.\r\n", ch);
-      return;
-    }
-
-  if ( !checkchem )
-    {
-      send_to_char( "&RSome chemicals to light would come in handy!\r\n", ch);
-      return;
-    }
-
-  the_chance = is_npc(ch) ? ch->top_level
-    : (int) (ch->pcdata->learned[gsn_makeglowrod]);
-
-  if ( number_percent( ) < the_chance )
-    {
-      send_to_char( "&GYou begin the long process of making a light.\r\n", ch);
-      act( AT_PLAIN, "$n takes $s tools and begins to work on something.", ch,
-	   NULL, argument , TO_ROOM );
-      add_timer ( ch , TIMER_DO_FUN , 10 , do_makeglowrod , SUB_PAUSE );
-      ch->dest_buf   = str_dup(arg);
-      return;
-    }
-  else
-    {
-      send_to_char("&RYou can't figure out how to fit the parts together.\r\n",ch);
-      learn_from_failure( ch, gsn_makeglowrod );
-    }
-}
-
-static void OnFinished( Character *ch )
-{
-  char arg[MAX_INPUT_LENGTH];
+  struct UserData *ud = (struct UserData*) userData;
   char buf[MAX_STRING_LENGTH];
-  int level = 0, the_chance = 0, strength = 0;
-  bool checktool = false;
-  bool checkbatt = false;
-  bool checkchem = false;
-  bool checkcirc = false;
-  bool checklens = false;
-  OBJ_DATA *obj = NULL;
-  OBJ_INDEX_DATA *pObjIndex = NULL;
-  vnum_t vnum = INVALID_VNUM;
-  long xpgain = 0;
+  OBJ_DATA *glowrod = args->Object;
 
-  if ( !ch->dest_buf )
-    return;
+  glowrod->item_type = ITEM_LIGHT;
+  SET_BIT( glowrod->wear_flags, ITEM_TAKE );
+  glowrod->weight = 3;
 
-  strcpy(arg, (const char*)ch->dest_buf);
-  DISPOSE( ch->dest_buf);
+  STRFREE( glowrod->name );
+  strcpy( buf, ud->ItemName );
+  strcat( buf, " glowrod");
+  glowrod->name = STRALLOC( buf );
 
-  level = is_npc(ch) ? ch->top_level : (int) (ch->pcdata->learned[gsn_makeglowrod]);
-  vnum = OBJ_VNUM_CRAFTING_GLOWROD;
+  strcpy( buf, ud->ItemName );
+  STRFREE( glowrod->short_descr );
+  glowrod->short_descr = STRALLOC( buf );
 
-  if ( ( pObjIndex = get_obj_index( vnum ) ) == NULL )
-    {
-      send_to_char( "&RThe item you are trying to create is missing from the database.\r\nPlease inform the administration of this error.\r\n", ch );
-      return;
-    }
-
-  for ( obj = ch->last_carrying; obj; obj = obj->prev_content )
-    {
-      if (obj->item_type == ITEM_TOOLKIT)
-        checktool = true;
-
-      if (obj->item_type == ITEM_BATTERY && checkbatt == false)
-        {
-          strength = obj->value[OVAL_BATTERY_CHARGE];
-          separate_obj( obj );
-          obj_from_char( obj );
-          extract_obj( obj );
-          checkbatt = true;
-        }
-      if (obj->item_type == ITEM_CHEMICAL)
-        {
-          separate_obj( obj );
-          obj_from_char( obj );
-          extract_obj( obj );
-          checkchem = true;
-        }
-      if (obj->item_type == ITEM_CIRCUIT && checkcirc == false)
-        {
-          separate_obj( obj );
-          obj_from_char( obj );
-          extract_obj( obj );
-          checkcirc = true;
-        }
-      if (obj->item_type == ITEM_LENS && checklens == false)
-        {
-          separate_obj( obj );
-          obj_from_char( obj );
-          extract_obj( obj );
-          checklens = true;
-        }
-    }
-
-  the_chance = is_npc(ch) ? ch->top_level
-    : (int) (ch->pcdata->learned[gsn_makeglowrod]) ;
-
-  if ( number_percent( ) > the_chance*2  || ( !checktool ) || ( !checklens ) || ( !checkbatt ) || ( !checkchem ) || ( !checkcirc) )
-    {
-      send_to_char( "&RJust as you are about to finish your work,\r\nyour newly created glowrod explodes in your hands...doh!\r\n", ch);
-      learn_from_failure( ch, gsn_makeglowrod );
-      return;
-    }
-
-  obj = create_object( pObjIndex, level );
-
-  obj->item_type = ITEM_LIGHT;
-  SET_BIT( obj->wear_flags, ITEM_TAKE );
-  obj->level = level;
-  obj->weight = 3;
-  STRFREE( obj->name );
-  strcpy( buf , arg );
-  strcat( buf , " glowrod");
-  obj->name = STRALLOC( buf );
-  strcpy( buf, arg );
-  STRFREE( obj->short_descr );
-  obj->short_descr = STRALLOC( buf );
-  STRFREE( obj->description );
+  STRFREE( glowrod->description );
   strcat( buf, " was carelessly misplaced here." );
-  obj->description = STRALLOC( buf );
-  obj->value[OVAL_LIGHT_POWER] = strength;
-  obj->cost = obj->value[OVAL_LIGHT_POWER];
+  glowrod->description = STRALLOC( capitalize( buf ) );
 
-  obj = obj_to_char( obj, ch );
-
-  send_to_char( "&GYou finish your work and hold up your newly created glowrod.&w\r\n", ch);
-  act( AT_PLAIN, "$n finishes making $s new glowrod.", ch,
-       NULL, NULL, TO_ROOM );
-
-  xpgain = UMIN( obj->cost*100 ,( exp_level(get_level( ch, ENGINEERING_ABILITY ) + 1) - exp_level(get_level( ch, ENGINEERING_ABILITY ) ) ) );
-  gain_exp(ch, ENGINEERING_ABILITY, xpgain );
-  ch_printf( ch , "You gain %d engineering experience.", xpgain );
-
-  learn_from_success( ch, gsn_makeglowrod );
+  glowrod->value[OVAL_LIGHT_POWER] = ud->Charge;
+  glowrod->cost = glowrod->value[OVAL_LIGHT_POWER];
 }
 
-static void OnAbort( Character *ch )
+static void FinishedCraftingHandler( void *userData, FinishedCraftingEventArgs *args )
 {
-  DISPOSE( ch->dest_buf );
-  send_to_char("&RYou are interupted and fail to finish your work.\r\n", ch);
+  struct UserData *ud = (struct UserData*) userData;
+  FreeUserData( ud );
+}
+
+static void AbortHandler( void *userData, AbortCraftingEventArgs *args )
+{
+  struct UserData *ud = (struct UserData*) userData;
+  FreeUserData( ud );
+}
+
+static void FreeUserData( struct UserData *ud )
+{
+  if( ud->ItemName )
+    {
+      DISPOSE( ud->ItemName );
+    }
+
+  DISPOSE( ud );
 }
