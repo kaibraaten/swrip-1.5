4c6ff3b505f5e755900cee3e614a07a0e6ddb20d
Kai Braaten <kai.braaten@gmail.com>
2016-03-20 20:48:54 +0000

    Refactored makelandmine to new crafting engine, but it doesn't check that the drink container is empty.

--- src/commands/makelandmine.c
+++ src/commands/makelandmine.c
@@ -1,235 +1,130 @@
 #include <string.h>
 #include "mud.h"
 #include "character.h"
+#include "craft.h"
 
-static void OnStart( Character *ch, char *argument );
-static void OnFinished( Character *ch );
-static void OnAbort( Character *ch );
+struct UserData
+{
+  int Strength;
+  int Weight;
+  int Level;
+  char *ItemName;
+};
+
+static void InterpretArgumentsHandler( void *userData, InterpretArgumentsEventArgs *args );
+static void MaterialFoundHandler( void *userData, MaterialFoundEventArgs *args );
+static void SetObjectStatsHandler( void *userData, SetObjectStatsEventArgs *args );
+static void FinishedCraftingHandler( void *userData, FinishedCraftingEventArgs *args );
+static void AbortHandler( void *userData, AbortCraftingEventArgs *args );
+static void FreeUserData( struct UserData *ud );
 
 void do_makelandmine( Character *ch, char *argument )
 {
-  switch( ch->substate )
+  struct UserData *data = NULL;
+  static const struct CraftingMaterial materials[] =
     {
-    default:
-      OnStart( ch, argument );
-      break;
+      { ITEM_TOOLKIT,         CRAFTFLAG_NONE },
+      { ITEM_BATTERY,         CRAFTFLAG_EXTRACT },
+      { ITEM_CIRCUIT,         CRAFTFLAG_EXTRACT },
+      { ITEM_DRINK_CON,       CRAFTFLAG_EXTRACT },
+      { ITEM_CHEMICAL,        CRAFTFLAG_EXTRACT },
+      { ITEM_NONE,            CRAFTFLAG_NONE },
+    };
+  CraftRecipe *recipe = AllocateCraftRecipe( gsn_makelandmine, materials,
+                                             25, OBJ_VNUM_CRAFTING_LANDMINE,
+					     CRAFTFLAG_NEED_WORKSHOP );
+  CraftingSession *session = AllocateCraftingSession( recipe, ch, argument );
 
-    case SUB_PAUSE:
-      ch->substate = SUB_NONE;
-      OnFinished( ch );
-      break;
+  AllocateMemory( data, struct UserData, 1 );
+  data->Level = IsNpc(ch) ? ch->top_level : (int) (ch->pcdata->learned[gsn_makelandmine]);
 
-    case SUB_TIMER_DO_ABORT:
-      ch->substate = SUB_NONE;
-      OnAbort( ch );
-      break;
+  AddInterpretArgumentsCraftingHandler( session, data, InterpretArgumentsHandler );
+  AddMaterialFoundCraftingHandler( session, data, MaterialFoundHandler );
+  AddSetObjectStatsCraftingHandler( session, data, SetObjectStatsHandler );
+  AddFinishedCraftingHandler( session, data, FinishedCraftingHandler );
+  AddAbortCraftingHandler( session, data, AbortHandler );
+
+  StartCrafting( session );
+}
+
+static void InterpretArgumentsHandler( void *userData, InterpretArgumentsEventArgs *args )
+{
+  Character *ch = GetEngineer( args->CraftingSession );
+  struct UserData *ud = (struct UserData*) userData;
+
+  if ( args->CommandArguments[0] == '\0' )
+    {
+      Echo( ch, "&RUsage: Makelandmine <name>\r\n&w" );
+      args->AbortSession = true;
+      return;
+    }
+
+  ud->ItemName = CopyString( args->CommandArguments );
+}
+
+static void MaterialFoundHandler( void *userData, MaterialFoundEventArgs *args )
+{
+  struct UserData *ud = (struct UserData*) userData;
+
+  if( args->Object->item_type == ITEM_DRINK_CON
+      && args->Object->value[OVAL_DRINK_CON_CURRENT_AMOUNT] > 0 )
+    {
+      /* FAIL! */
+    }
+
+  if( args->Object->item_type == ITEM_CHEMICAL )
+    {
+      ud->Strength = urange( 10, args->Object->value[OVAL_CHEMICAL_STRENGTH], ud->Level * 5 );
+      ud->Weight = args->Object->weight;
     }
 }
 
-static void OnStart( Character *ch, char *argument )
+static void SetObjectStatsHandler( void *userData, SetObjectStatsEventArgs *args )
 {
-  char arg[MAX_INPUT_LENGTH];
-  int the_chance = 0;
-  bool checktool = false;
-  bool checkdrink = false;
-  bool checkbatt = false;
-  bool checkchem = false;
-  bool checkcirc = false;
-  Object *obj = NULL;
-
-  strcpy( arg , argument );
-
-  if ( arg[0] == '\0' )
-    {
-      SendToCharacter( "&RUsage: Makelandmine <name>\r\n&w", ch);
-      return;
-    }
-
-  if ( !IsBitSet( ch->in_room->room_flags, ROOM_FACTORY ) )
-    {
-      SendToCharacter( "&RYou need to be in a factory or workshop to do that.\r\n", ch);
-      return;
-    }
-
-  for ( obj = ch->last_carrying; obj; obj = obj->prev_content )
-    {
-      if (obj->item_type == ITEM_TOOLKIT)
-	checktool = true;
-      if (obj->item_type == ITEM_DRINK_CON
-	  && obj->value[OVAL_DRINK_CON_CURRENT_AMOUNT] == 0 )
-	checkdrink = true;
-      if (obj->item_type == ITEM_BATTERY)
-	checkbatt = true;
-      if (obj->item_type == ITEM_CIRCUIT)
-	checkcirc = true;
-      if (obj->item_type == ITEM_CHEMICAL)
-	checkchem = true;
-    }
-
-  if ( !checktool )
-    {
-      SendToCharacter( "&RYou need toolkit to make a landmine.\r\n", ch);
-      return;
-    }
-
-  if ( !checkdrink )
-    {
-      SendToCharacter( "&RYou will need an empty drink container to mix and hold the chemicals.\r\n", ch);
-      return;
-    }
-
-  if ( !checkbatt )
-    {
-      SendToCharacter( "&RYou need a small battery for the detonator.\r\n", ch);
-      return;
-    }
-
-  if ( !checkcirc )
-    {
-      SendToCharacter( "&RYou need a small circuit for the detonator.\r\n", ch);
-      return;
-    }
-
-  if ( !checkchem )
-    {
-      SendToCharacter( "&RSome explosive chemicals would come in handy!\r\n", ch);
-      return;
-    }
-
-  the_chance = IsNpc(ch) ? ch->top_level
-    : (int) (ch->pcdata->learned[gsn_makelandmine]);
-
-  if ( GetRandomPercent() < the_chance )
-    {
-      SendToCharacter( "&GYou begin the long process of making a landmine.\r\n", ch);
-      Act( AT_PLAIN, "$n takes $s tools and a drink container and begins to work on something.", ch,
-	   NULL, NULL , TO_ROOM );
-      AddTimerToCharacter( ch , TIMER_CMD_FUN , 25 , do_makelandmine , SUB_PAUSE );
-      ch->dest_buf   = CopyString(arg);
-    }
-  else
-    {
-      SendToCharacter("&RYou can't figure out how to fit the parts together.\r\n",ch);
-      LearnFromFailure( ch, gsn_makelandmine );
-    }
-}
-
-static void OnFinished( Character *ch )
-{
-  char arg[MAX_INPUT_LENGTH];
+  struct UserData *ud = (struct UserData*) userData;
   char buf[MAX_STRING_LENGTH];
-  int level = 0;
-  int the_chance = 0;
-  int strength = 0;
-  int weight = 0;
-  bool checktool = false;
-  bool checkdrink = false;
-  bool checkbatt = false;
-  bool checkchem = false;
-  bool checkcirc = false;
-  Object *obj = NULL;
-  ProtoObject *pObjIndex = NULL;
-  vnum_t vnum = INVALID_VNUM;
-  long xpgain = 0;
+  Object *landmine = args->Object;
 
-  if ( !ch->dest_buf )
-    return;
+  SetBit( landmine->wear_flags, ITEM_HOLD );
+  SetBit( landmine->wear_flags, ITEM_TAKE );
+  landmine->weight = ud->Weight;
 
-  strcpy(arg, (const char*)ch->dest_buf);
-  FreeMemory( ch->dest_buf);
+  FreeMemory( landmine->name );
+  strcpy( buf, ud->ItemName );
+  strcat( buf, " landmine");
+  landmine->name = CopyString( buf );
 
-  level = IsNpc(ch) ? ch->top_level : (int) (ch->pcdata->learned[gsn_makelandmine]);
-  vnum = OBJ_VNUM_CRAFTING_LANDMINE;
+  strcpy( buf, ud->ItemName );
+  FreeMemory( landmine->short_descr );
+  landmine->short_descr = CopyString( buf );
 
-  if ( ( pObjIndex = GetProtoObject( vnum ) ) == NULL )
-    {
-      SendToCharacter( "&RThe item you are trying to create is missing from the database.\r\nPlease inform the administration of this error.\r\n", ch );
-      return;
-    }
-
-  for ( obj = ch->last_carrying; obj; obj = obj->prev_content )
-    {
-      if (obj->item_type == ITEM_TOOLKIT)
-        checktool = true;
-      if (obj->item_type == ITEM_DRINK_CON && checkdrink == false
-	  && obj->value[OVAL_DRINK_CON_CURRENT_AMOUNT] == 0 )
-        {
-          checkdrink = true;
-          SeparateOneObjectFromGroup( obj );
-          ObjectFromCharacter( obj );
-          ExtractObject( obj );
-        }
-      if (obj->item_type == ITEM_BATTERY && checkbatt == false)
-        {
-          SeparateOneObjectFromGroup( obj );
-          ObjectFromCharacter( obj );
-          ExtractObject( obj );
-          checkbatt = true;
-        }
-      if (obj->item_type == ITEM_CHEMICAL)
-        {
-          strength = urange( 10, obj->value[OVAL_CHEMICAL_STRENGTH], level * 5 );
-          weight = obj->weight;
-          SeparateOneObjectFromGroup( obj );
-          ObjectFromCharacter( obj );
-          ExtractObject( obj );
-          checkchem = true;
-        }
-      if (obj->item_type == ITEM_CIRCUIT && checkcirc == false)
-        {
-          SeparateOneObjectFromGroup( obj );
-          ObjectFromCharacter( obj );
-          ExtractObject( obj );
-          checkcirc = true;
-        }
-    }
-
-  the_chance = IsNpc(ch) ? ch->top_level
-    : (int) (ch->pcdata->learned[gsn_makelandmine]) ;
-
-  if ( GetRandomPercent() > the_chance*2  || ( !checktool ) || ( !checkdrink ) || ( !checkbatt ) || ( !checkchem ) || ( !checkcirc) )
-    {
-      SendToCharacter( "&RJust as you are about to finish your work,\r\nyour newly created landmine explodes in your hands...doh!\r\n", ch);
-      LearnFromFailure( ch, gsn_makelandmine );
-      return;
-    }
-
-  obj = CreateObject( pObjIndex, level );
-
-  obj->item_type = ITEM_LANDMINE;
-  SetBit( obj->wear_flags, ITEM_HOLD );
-  SetBit( obj->wear_flags, ITEM_TAKE );
-  obj->level = level;
-  obj->weight = weight;
-  FreeMemory( obj->name );
-  strcpy( buf , arg );
-  strcat( buf , " landmine");
-  obj->name = CopyString( buf );
-  strcpy( buf, arg );
-  FreeMemory( obj->short_descr );
-  obj->short_descr = CopyString( buf );
-  FreeMemory( obj->description );
+  FreeMemory( landmine->description );
   strcat( buf, " was carelessly misplaced here." );
-  obj->description = CopyString( buf );
-  obj->value[OVAL_EXPLOSIVE_MIN_DMG] = strength/2;
-  obj->value[OVAL_EXPLOSIVE_MAX_DMG] = strength;
-  obj->cost = obj->value[OVAL_EXPLOSIVE_MAX_DMG]*5;
+  landmine->description = CopyString( Capitalize( buf ) );
 
-  obj = ObjectToCharacter( obj, ch );
-
-  SendToCharacter( "&GYou finish your work and hold up your newly created landmine.&w\r\n", ch);
-  Act( AT_PLAIN, "$n finishes making $s new landmine.", ch,
-       NULL, NULL, TO_ROOM );
-
-  xpgain = umin( obj->cost*50 ,( GetRequiredXpForLevel(GetAbilityLevel( ch, ENGINEERING_ABILITY ) + 1) - GetRequiredXpForLevel(GetAbilityLevel( ch, ENGINEERING_ABILITY ) ) ) );
-  GainXP(ch, ENGINEERING_ABILITY, xpgain );
-  Echo( ch , "You gain %d engineering experience.", xpgain );
-
-  LearnFromSuccess( ch, gsn_makelandmine );
+  landmine->value[OVAL_EXPLOSIVE_MIN_DMG] = ud->Strength / 2;
+  landmine->value[OVAL_EXPLOSIVE_MAX_DMG] = ud->Strength;
+  landmine->cost = landmine->value[OVAL_EXPLOSIVE_MAX_DMG] * 5;
 }
 
-static void OnAbort( Character *ch )
+static void FinishedCraftingHandler( void *userData, FinishedCraftingEventArgs *args )
 {
-  FreeMemory( ch->dest_buf );
-  SendToCharacter("&RYou are interupted and fail to finish your work.\r\n", ch);
+  struct UserData *ud = (struct UserData*) userData;
+  FreeUserData( ud );
+}
+
+static void AbortHandler( void *userData, AbortCraftingEventArgs *args )
+{
+  struct UserData *ud = (struct UserData*) userData;
+  FreeUserData( ud );
+}
+
+static void FreeUserData( struct UserData *ud )
+{
+  if( ud->ItemName )
+    {
+      FreeMemory( ud->ItemName );
+    }
+
+  FreeMemory( ud );
 }
