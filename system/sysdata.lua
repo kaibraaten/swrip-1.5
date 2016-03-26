e6e4151869c9fa8cf93d7ef7c180f078f1521067
Kai Braaten <kai.braaten@gmail.com>
2016-03-26 12:36:20 +0000

    sdf

---
+++ system/sysdata.lua
@@ -0,0 +1,29 @@
+-- SystemData
+-- Last saved Saturday 26-Mar-2016 11:51:35
+
+SystemDataEntry
+{
+   WriteMailFree = 1, 
+   ForcePc = 101, 
+   StunRegular = 0, 
+   MsetPlayer = 104, 
+   ModifyProto = 102, 
+   LogChannelLevel = 103, 
+   StunModPvP = 50, 
+   OverridePrivateFlag = 103, 
+   ReadAllMail = 103, 
+   TakeOthersMail = 102, 
+   NoNameResolving = 0, 
+   DisableHunger = 1, 
+   DamModPvP = 103, 
+   ReadMailFree = 101, 
+   AllTimeMaxPlayers = 1, 
+   DamModEvP = 103, 
+   AllTimeMaxPlayersTime = "Sat Mar 26 11:51:35 2016", 
+   SaveFrequency = 20, 
+   BuildChannelLevel = 102, 
+   SaveFlags = 4045, 
+   DamModEvE = 103, 
+   DamModPvE = 103, 
+   WaitForAuth = 1, 
+}
