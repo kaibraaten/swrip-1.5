600120962a3e29a98fb581b610fa83a2b724a35b
Kai Braaten <kai.braaten@gmail.com>
2016-03-27 11:02:43 +0000

    sdf

---
+++ clans/the_empire.lua
@@ -0,0 +1,21 @@
+-- The Empire
+-- Last saved Sunday 27-Mar-2016 10:52:42
+
+ClanEntry
+{
+   Type = 0, 
+   Funds = 0, 
+   MobDeaths = 0, 
+   Leader = "", 
+   Members = 
+   {
+   }, 
+   Number2 = "", 
+   Number1 = "", 
+   Name = "The Empire", 
+   NumberOfVehicles = 0, 
+   NumberOfSpacecraft = 2, 
+   PlayerKills = 0, 
+   PlayerDeaths = 0, 
+   MobKills = 0, 
+}
---
+++ clans/the_rebel_alliance.lua
@@ -0,0 +1,30 @@
+-- The Rebel Alliance
+-- Last saved Sunday 27-Mar-2016 11:02:06
+
+ClanEntry
+{
+   Type = 0, 
+   Funds = 0, 
+   MobDeaths = 0, 
+   Leader = "", 
+   Members = 
+   {
+      [1] = 
+      {
+         Name = "Naty", 
+         PlayerKills = 0, 
+         Ability = "combat", 
+         Level = 2, 
+         MemberSince = "[03|27|2016]", 
+         PlayerDeaths = 0, 
+      }, 
+   }, 
+   Number2 = "", 
+   Number1 = "", 
+   Name = "The Rebel Alliance", 
+   NumberOfVehicles = 0, 
+   NumberOfSpacecraft = 0, 
+   PlayerKills = 0, 
+   PlayerDeaths = 0, 
+   MobKills = 0, 
+}
