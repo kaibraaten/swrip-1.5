ec11db003b0399736aa16e081278d348a5d96111
Kai Braaten <kai.braaten@gmail.com>
2016-10-08 09:04:26 +0000

    IsNullOrEmpty()

--- src/commands/switch.c
+++ src/commands/switch.c
@@ -8,7 +8,7 @@ void do_switch( Character *ch, char *argument )
 
   OneArgument( argument, arg );
 
-  if ( arg[0] == '\0' )
+  if ( IsNullOrEmpty( arg ) )
     {
       SendToCharacter( "Switch into whom?\r\n", ch );
       return;
