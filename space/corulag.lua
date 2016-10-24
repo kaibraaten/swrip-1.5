9df199c155b2329514707a6e90db5971a04f036b
Kai Braaten <kai.braaten@gmail.com>
2016-10-24 08:54:25 +0000

    New spaceobject.

---
+++ space/corulag.lua
@@ -0,0 +1,18 @@
+-- Corulag
+-- Last saved Monday 24-Oct-2016 08:53:27
+
+SpaceobjectEntry
+{
+   LandingSites = 
+   {
+   }, 
+   Type = "sun", 
+   Name = "Corulag", 
+   Position = 
+   {
+      Y = 0, 
+      X = 0, 
+      Z = 0, 
+   }, 
+   Gravity = 0, 
+}
