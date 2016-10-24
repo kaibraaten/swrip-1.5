5d07a9cb6b1d10d56bab1210c275977bb4cd7871
Kai Braaten <kai.braaten@gmail.com>
2016-10-24 19:25:28 +0000

    sdf

---
+++ space/kuat.lua
@@ -0,0 +1,25 @@
+-- Kuat
+-- Last saved Monday 24-Oct-2016 19:21:02
+
+SpaceobjectEntry
+{
+   LandingSites = 
+   {
+      [0] = 
+      {
+         DockVnum = 23000, 
+         Name = "Kuat Starport", 
+         IsSecret = 0, 
+      }, 
+   }, 
+   Type = "planet", 
+   Name = "Kuat", 
+   Position = 
+   {
+      Y = 0, 
+      X = 0, 
+      Z = -300000, 
+   }, 
+   Planet = "Kuat", 
+   Gravity = 100, 
+}
---
+++ space/ryloth.lua
@@ -0,0 +1,25 @@
+-- Ryloth
+-- Last saved Monday 24-Oct-2016 19:10:06
+
+SpaceobjectEntry
+{
+   LandingSites = 
+   {
+      [0] = 
+      {
+         DockVnum = 800, 
+         Name = "Kala'uun Starport", 
+         IsSecret = 0, 
+      }, 
+   }, 
+   Type = "planet", 
+   Name = "Ryloth", 
+   Position = 
+   {
+      Y = 0, 
+      X = 0, 
+      Z = 300000, 
+   }, 
+   Planet = "Ryloth", 
+   Gravity = 100, 
+}
