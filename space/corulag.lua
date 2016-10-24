fd0710edb91762f056a9882f0a5331bfcd979b17
Kai Braaten <kai.braaten@gmail.com>
2016-10-24 09:09:10 +0000

    sdf

--- space/corulag.lua
+++ space/corulag.lua
@@ -1,18 +1,31 @@
 -- Corulag
--- Last saved Monday 24-Oct-2016 08:53:27
+-- Last saved Monday 24-Oct-2016 09:00:35
 
 SpaceobjectEntry
 {
    LandingSites = 
    {
+      [1] = 
+      {
+         DockVnum = 21821, 
+         Name = "Imperial Academy", 
+         IsSecret = 0, 
+      }, 
+      [0] = 
+      {
+         DockVnum = 21944, 
+         Name = "Gzenah Landing Facility", 
+         IsSecret = 0, 
+      }, 
    }, 
-   Type = "sun", 
+   Type = "planet", 
    Name = "Corulag", 
    Position = 
    {
-      Y = 0, 
-      X = 0, 
+      Y = 3000000, 
+      X = -500000, 
       Z = 0, 
    }, 
-   Gravity = 0, 
+   Planet = "Corulag", 
+   Gravity = 100, 
 }
