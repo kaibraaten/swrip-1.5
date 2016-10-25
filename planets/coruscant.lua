f042d77ff7498c8ff1cadb5ec4204dcd15a14f71
Kai Braaten <kai.braaten@gmail.com>
2016-10-25 19:58:09 +0000

    sdf

--- planets/coruscant.lua
+++ planets/coruscant.lua
@@ -1,5 +1,5 @@
 -- Coruscant
--- Last saved Monday 24-Oct-2016 22:10:59
+-- Last saved Tuesday 25-Oct-2016 19:29:49
 
 PlanetEntry
 {
@@ -14,11 +14,11 @@ PlanetEntry
    GovernedBy = "The Empire", 
    Areas = 
    {
-      [1] = "grand_towers.are", 
+      [1] = "monument_plaza.are", 
       [2] = "corus_jail.are", 
       [3] = "corus_pt2.are", 
-      [4] = "monument_plaza.are", 
-      [5] = "senate.are", 
-      [6] = "coruscant_streets.are", 
+      [4] = "senate.are", 
+      [5] = "coruscant_streets.are", 
+      [6] = "grand_towers.are", 
    }, 
 }
---
+++ planets/hoth.lua
@@ -0,0 +1,14 @@
+-- Hoth
+-- Last saved Tuesday 25-Oct-2016 19:56:33
+
+PlanetEntry
+{
+   PopulationSupport = 0, 
+   Name = "Hoth", 
+   Spaceobject = "Hoth", 
+   BaseValue = 0, 
+   Areas = 
+   {
+      [1] = "hoth.are", 
+   }, 
+}
