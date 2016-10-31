444860b71225919be383d0e5a943a64eb63f548e
Kai Braaten <kai.braaten@gmail.com>
2016-10-31 13:57:26 +0000

    sdf

---
+++ boards/immortal.lua
@@ -0,0 +1,33 @@
+-- immortal
+-- Last saved Monday 31-Oct-2016 13:56:40
+
+BoardEntry
+{
+   PostGroup = "", 
+   Type = "Note", 
+   MinRemoveLevel = 105, 
+   MinPostLevel = 101, 
+   BoardObjectVnum = 100, 
+   ExtraRemovers = "", 
+   ReadGroup = "", 
+   Name = "immortal", 
+   Notes = 
+   {
+      [1] = 
+      {
+         Abstentions = "", 
+         Sender = "Kai", 
+         Voting = false, 
+         Date = "Mon Oct 31 13:15:55 2016", 
+         Text = "Welcome to this refactored and improved version of SWRiP 1.0.1!\
+\r", 
+         NoVotes = "", 
+         YesVotes = "", 
+         ToList = "All", 
+         Subject = "Welcome to SWRiP 1.5!", 
+      }, 
+   }, 
+   ExtraReaders = "", 
+   MaxPosts = 1000, 
+   MinReadLevel = 101, 
+}
