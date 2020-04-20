a953383c86211d0c0d0d7b5286762a7e8ecd4ff6
Kai Braaten <kai.braaten@gmail.com>
2020-04-20 20:06:24 +0200

    do_renameship now works as expected. #4.

--- src/commands/renameship.cpp
+++ src/commands/renameship.cpp
@@ -1,3 +1,4 @@
+#include <filesystem>
 #include "mud.hpp"
 #include "ship.hpp"
 #include "character.hpp"
@@ -5,39 +6,46 @@
 #include "room.hpp"
 #include "repos/shiprepository.hpp"
 
-void do_renameship( Character *ch, std::string argument )
+void do_renameship(Character* ch, std::string argument)
 {
-  std::shared_ptr<Ship> ship;
-  std::shared_ptr<Clan> clan;
-  
-  if ( (ship = GetShipFromCockpit( ch->InRoom->Vnum ) ) == NULL)
-    {
-      ch->Echo("You must be in the cockpit of a ship to do that!\r\n");
-      return;
-    }
+    constexpr auto COST_TO_RENAME = 50000;
 
-  if( ( (clan = GetClan(ship->Owner)) == NULL ) || StrCmp( clan->Leadership.Leader, ch->Name ) )
-    if( StrCmp( ship->Owner, ch->Name ) )
-      {
-        ch->Echo("&RImperial Database: &WYou do not own this ship.\r\n");
+    std::shared_ptr<Ship> ship = GetShipFromCockpit(ch->InRoom->Vnum);
+    
+    if (ship == nullptr)
+    {
+        ch->Echo("You must be in the cockpit of a ship to do that!\r\n");
         return;
-      }
+    }
 
-  if( GetShipAnywhere( argument ) != NULL )
+    std::shared_ptr<Clan> clan = GetClan(ship->Owner);
+
+    if (clan == nullptr || StrCmp(clan->Leadership.Leader, ch->Name))
     {
-      ch->Echo("&RImperial Database: &WA ship of that name already exists.\r\n");
-      return;
+        if (StrCmp(ship->Owner, ch->Name))
+        {
+            ch->Echo("&RImperial Database: &WYou do not own this ship.\r\n");
+            return;
+        }
+    }
+
+    if (GetShipAnywhere(argument) != NULL)
+    {
+        ch->Echo("&RImperial Database: &WA ship of that name already exists.\r\n");
+        return;
     }
 
 
-  if( ch->Gold < 50000 )
+    if (ch->Gold < COST_TO_RENAME)
     {
-      ch->Echo("&RImperial Database: &WYou do not have enough credits for this request.\r\n");
-      return;
+        ch->Echo("&RImperial Database: &WYou do not have enough credits for this request.\r\n");
+        return;
     }
 
-  ch->Gold -= 50000;
-  ship->PersonalName = argument;
-  Ships->Save(ship);
-  ch->Echo("&RImperial Database: &WTransaction Complete. Name changed.");
+    ch->Gold -= COST_TO_RENAME;
+    std::string oldFilename = GetShipFilename(ship);
+    std::filesystem::rename(oldFilename, oldFilename + ".bak");
+    ship->PersonalName = argument;
+    Ships->Save(ship);
+    ch->Echo("&RImperial Database: &WTransaction Complete. Name changed.");
 }
