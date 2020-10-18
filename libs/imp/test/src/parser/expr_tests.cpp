fa73f3fa632d950d8bf0d4a52a21fbceebc9699b
Kai Braaten <kai.braaten@gmail.com>
2020-10-18 11:05:14 +0200

    Starting expression tests.

---
+++ libs/imp/test/src/parser/expr_tests.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <list>
+#include <utility>
+
+#include "imp/scanner/tokenkind.hpp"
+#include "imp/scanner/scanner.hpp"
+#include "imp/scanner/token.hpp"
+#include "imp/parser/expr.hpp"
+
+using namespace Imp;
