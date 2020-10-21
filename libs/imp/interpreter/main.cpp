89fab24b81a42e0636bd0db0dd8ed131b8180f87
Kai Braaten <kai@ceris.no>
2020-10-21 11:44:01 +0200

    Interpreter can now read files.

--- libs/imp/interpreter/main.cpp
+++ libs/imp/interpreter/main.cpp
@@ -1,15 +1,48 @@
+#include <iostream>
+#include <fstream>
 #include <imp/scanner/scanner.hpp>
 #include <imp/runtime/standardlibrary.hpp>
 #include <imp/parser/program.hpp>
 
-int main(int arc, char *argv[])
+static std::list<std::string> LoadScript(const std::string &filename);
+
+int main(int argc, char *argv[])
 {
-    std::list<std::string> code = { "print('Hello world!')" };
-    auto scanner = std::make_shared<Imp::Scanner>(code);
-    auto lib = std::make_shared<Imp::StandardLibrary>();
-    auto globalScope = std::make_shared<Imp::RuntimeScope>(lib);
+    std::cout << "Imp interpreter version 1.0" << std::endl;
+    
+    if(argc < 2)
+    {
+        std::cout << "Usage: runimp <filename.py>" << std::endl;
+        return 0;
+    }
 
-    auto prog = Imp::Program::Parse(scanner);
+    try
+    {
+        auto code = LoadScript(argv[1]);
+        auto scanner = std::make_shared<Imp::Scanner>(code);
+        auto lib = std::make_shared<Imp::StandardLibrary>();
+        auto globalScope = std::make_shared<Imp::RuntimeScope>(lib);
 
-    prog->Eval(globalScope);
+        auto prog = Imp::Program::Parse(scanner);
+
+        prog->Eval(globalScope);
+    }
+    catch(const std::exception &ex)
+    {
+        std::cerr << ex.what() << std::endl;
+    }
+}
+
+static std::list<std::string> LoadScript(const std::string &filename)
+{
+    std::list<std::string> code;
+    std::ifstream file(filename);
+    std::string line;
+
+    while(std::getline(file, line))
+    {
+        code.push_back(line);
+    }
+
+    return code;
 }
