a0c5d06fa986340c98dc104e105048126470ca72
Kai Braaten <kai.braaten@gmail.com>
2020-12-14 14:43:04 +0100

    Fix for running on Windows.

--- winstart.bat
+++ winstart.bat
@@ -1,1 +1,6 @@
-out\build\x64-Debug\bin\swrip 7000
+@echo off
+
+:: Make a copy of executable and run that copy. Why? Because then we can compile
+:: and link a new executable while the mud is still running.
+copy out\build\x64-Debug\bin\swrip.exe out\build\x64-Debug\bin\swrip-win.exe >NUL
+out\build\x64-Debug\bin\swrip-win 7000
